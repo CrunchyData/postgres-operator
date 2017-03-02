7169bd61a4dc5c0e9a03855c59fc46c3689f86fc
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-03-02 08:50:20 -0600

    update tpr objects

---
+++ examples/tpr/cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright 2016 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+kubectl delete CrunchyCluster my-new-crunchy-cluster-object
+kubectl delete CrunchyDatabase my-new-crunchy-database-object
+
+kubectl delete thirdpartyresource crunchy-cluster.crunchydata.com
+kubectl delete thirdpartyresource crunchy-database.crunchydata.com
+
