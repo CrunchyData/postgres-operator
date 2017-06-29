2806493da287ad97d7ae5840597d218a503fd127
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-06-29 16:17:40 -0500

    add policy processor to list of watchers

---
+++ operator/cluster/policies.go
@@ -0,0 +1,65 @@
+/*
+ Copyright 2017 Crunchy Data Solutions, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package cluster
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"os"
+
+	//"github.com/crunchydata/postgres-operator/operator/util"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
+	"k8s.io/client-go/pkg/watch"
+	"k8s.io/client-go/rest"
+)
+
+func ProcessPolicies(clientset *kubernetes.Clientset, tprclient *rest.RESTClient, stopchan chan struct{}, namespace string) {
+
+	lo := v1.ListOptions{LabelSelector: "pg-cluster"}
+	fw, err := clientset.Deployments(namespace).Watch(lo)
+	if err != nil {
+		log.Error("fatal error in ProcessPolicies " + err.Error())
+		os.Exit(2)
+	}
+
+	_, err4 := watch.Until(0, fw, func(event watch.Event) (bool, error) {
+		log.Infof("got a processpolicies watch event %v\n", event.Type)
+
+		switch event.Type {
+		case watch.Added:
+			deployment := event.Object.(*v1beta1.Deployment)
+			log.Infof("deployment processpolicy added=%s\n", deployment.Name)
+		case watch.Deleted:
+			deployment := event.Object.(*v1beta1.Deployment)
+			log.Infof("deployment processpolicy deleted=%s\n", deployment.Name)
+		case watch.Error:
+			log.Infof("deployment processpolicy error event")
+		case watch.Modified:
+			deployment := event.Object.(*v1beta1.Deployment)
+			log.Infof("deployment processpolicy modified=%s\n", deployment.Name)
+		default:
+			log.Infoln("processpolices unknown watch event %v\n", event.Type)
+		}
+
+		return false, nil
+	})
+
+	if err4 != nil {
+		log.Error("error in ProcessPolicies " + err4.Error())
+	}
+
+}
