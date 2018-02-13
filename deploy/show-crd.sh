c646aeaf7cb0db17a443bcf99719d36d9e35554b
Jeff McCormick <jeff.mccormick@crunchydata.com>
2018-02-13 11:55:29 -0500

    remove -x from scripts

--- deploy/show-crd.sh
+++ deploy/show-crd.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 # Copyright 2016 Crunchy Data Solutions, Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
