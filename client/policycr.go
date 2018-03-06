e028b2c7431cc842c18daaf45669ca877bdc952f
Jeff McCormick <jeff.mccormick@crunchydata.com>
2018-03-06 10:12:33 -0600

    update copyrights based on new guidance

--- client/policycr.go
+++ client/policycr.go
@@ -1,7 +1,7 @@
 package client
 
 /*
-Copyright 2018 The Kubernetes Authors.
+Copyright 2017-2018 Crunchy Data Solutions.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- golang-examples/createsecret.go
+++ golang-examples/createsecret.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 The Kubernetes Authors.
+Copyright 2018 Crunchy Data Solutions
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
