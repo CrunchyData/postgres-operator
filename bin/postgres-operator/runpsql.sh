1937f8f63dbb670b58543bb524655483f347dfc1
Jeff McCormick <jeff.mccormick@crunchydata.com>
2018-03-15 13:48:28 -0500

    remove password printing from runpsql script

--- bin/postgres-operator/runpsql.sh
+++ bin/postgres-operator/runpsql.sh
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo $1 is the password
-echo $2 is the host ip
+#echo $1 is the password
+#echo $2 is the host ip
 
 SLEEP_TIME=2 
 FAILURES=0
