1b027e9fc9f4e83cf7c8a45a01b913168286a023
jmccormick2001 <jeff.mccormick@crunchydata.com>
2018-11-14 16:27:14 -0500

    fix pre-pull script back to a normal pull

--- bin/pre-pull-crunchy-containers.sh
+++ bin/pre-pull-crunchy-containers.sh
@@ -13,15 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-gcloud auth login
-gcloud config set project container-suite
-
-REGISTRY='us.gcr.io/container-suite'
-LOCALREGISTRY=cortado-k1:5000/crunchydata
-CCP_IMAGE_TAG=centos7-11.0-2.2.0-rc6
 for CNAME in crunchy-scheduler crunchy-postgres crunchy-collect crunchy-grafana crunchy-prometheus crunchy-backup
 do
-	docker pull us.gcr.io/container-suite/$CNAME:$CCP_IMAGE_TAG
-	docker tag $REGISTRY/$CNAME:$CCP_IMAGE_TAG $LOCALREGISTRY/$CNAME:$CCP_IMAGE_TAG
-	docker push $LOCALREGISTRY/$CNAME:$CCP_IMAGE_TAG
+	docker pull crunchydata/$CNAME:$CCP_IMAGE_TAG
 done
