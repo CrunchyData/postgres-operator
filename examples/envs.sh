0896e446f9d3434d0aa0c2a3d0ff9a503d970646
jmccormick2001 <jeff.mccormick@crunchydata.com>
2019-01-14 15:07:04 -0600

    update envs

--- examples/envs.sh
+++ examples/envs.sh
@@ -14,12 +14,9 @@ export PGO_CA_CERT=$COROOT/conf/postgres-operator/server.crt
 export PGO_CLIENT_CERT=$COROOT/conf/postgres-operator/server.crt
 export PGO_CLIENT_KEY=$COROOT/conf/postgres-operator/server.key
 
-# for crunchy-scheduler startup
-export CCP_IMAGE_PREFIX=crunchydata
-export CCP_IMAGE_TAG=centos7-11.1-2.3.0
-
 # useful aliases
 alias setip='export CO_APISERVER_URL=https://`kubectl get service postgres-operator -o=jsonpath="{.spec.clusterIP}"`:8443'
 alias alog='kubectl logs `kubectl get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c apiserver'
 alias olog='kubectl logs `kubectl get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c operator'
+alias slog='kubectl logs `kubectl get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c scheduler'
 
