86c420306b87f08e075a01c184a6eba57af608db
Andrew L'Ecuyer <andrew.lecuyer@crunchydata.com>
2019-02-07 21:20:06 -0600

    A pgBackRest restore can now be reattempted after a failed restore attempt using the 'pgo restore' command

--- pgo/cmd/restore.go
+++ pgo/cmd/restore.go
@@ -41,7 +41,7 @@ var restoreCmd = &cobra.Command{
 		if len(args) == 0 {
 			fmt.Println(`Error: You must specify the cluster name to restore from.`)
 		} else {
-			fmt.Println("Warning:  stopping this database and creating a new primary is part of the restore workflow!")
+			fmt.Println("Warning:  If currently running, the primary database in this cluster will be stopped and recreated as part of this workflow!")
 			if util.AskForConfirmation(NoPrompt, "") {
 				restore(args, Namespace)
 			} else {
