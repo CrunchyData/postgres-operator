622b55ab123490b48fb3eafe231a7dafce6d5439
Paul Cahoon <paul.cahoon@crunchydata.com>
2019-02-22 14:54:35 -0500

    added the ability to generate the pgo cli markdown for inclusion into the documentation (#601)
    * updated links to gh-pages and added generated cli documents
    
    * add the ability to generate the pgo cli markdown for inclusion into the  documentation
    
    * removed extra go get line

---
+++ pgo/generatedocs.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"path/filepath"
+	"path"
+	"strings"
+	"github.com/spf13/cobra/doc"
+	"github.com/crunchydata/postgres-operator/pgo/cmd"
+	
+)
+
+const fmTemplate = `---
+title: "%s"
+---
+`
+
+func main() {
+	
+	fmt.Println("generate CLI markdown")
+
+	filePrepender := func(filename string) string {
+		// now := time.Now().Format(time.RFC3339)
+		name := filepath.Base(filename)
+		base := strings.TrimSuffix(name, path.Ext(name))
+		fmt.Println(base);
+		// url := "/commands/" + strings.ToLower(base) + "/"
+		return fmt.Sprintf(fmTemplate, base)
+	}
+
+	linkHandler := func(name string) string {
+		base := strings.TrimSuffix(name, path.Ext(name))
+		return "/commands/" + strings.ToLower(base) + "/"
+	}
+
+	err := doc.GenMarkdownTreeCustom(cmd.RootCmd, "./", filePrepender, linkHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
