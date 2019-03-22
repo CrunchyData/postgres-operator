f76baa7a805e3a08ae55e9461f5af4a70b92654e
jmccormick2001 <jeff.mccormick@crunchydata.com>
2019-03-22 14:45:34 -0400

    fix pgbouncer backrest restore bug and update to rc5 version string

--- operator/backrest/restore.go
+++ operator/backrest/restore.go
@@ -440,7 +440,8 @@ func CreateRestoredDeployment(restclient *rest.RESTClient, cluster *crv1.Pgclust
 		CollectAddon:            operator.GetCollectAddon(clientset, namespace, &cluster.Spec),
 		BadgerAddon:             operator.GetBadgerAddon(clientset, namespace, &cluster.Spec),
 		PgbackrestEnvVars:       operator.GetPgbackrestEnvVars(cluster.Spec.UserLabels[util.LABEL_BACKREST], cluster.Spec.Name, restoreToName, cluster.Spec.Port),
-		PgbouncerEnvVars:        operator.GetPgbouncerEnvVar(cluster.Spec.UserLabels[util.LABEL_PGBOUNCER_PASS]),
+		//PgbouncerEnvVars:        operator.GetPgbouncerEnvVar(cluster.Spec.UserLabels[util.LABEL_PGBOUNCER_PASS]),
+		PgbouncerEnvVars: "",
 	}
 
 	log.Debug("collectaddon value is [" + deploymentFields.CollectAddon + "]")
