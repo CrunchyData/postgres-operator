602b61d43e247dba4b83c0abcc422fb8cc9065a8
FlamingDumpster <931556+flamingdumpster@users.noreply.github.com>
2019-03-28 09:53:54 -0400

    Updates to get example running in release 4.0 (#662)

--- examples/create-by-resource/run.sh
+++ examples/create-by-resource/run.sh
@@ -15,19 +15,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# A namespace that exists in NAMESPACE env var - see examples/envs.sh
+export NS=pgouser1
+
 # remove any existing resources from a previous run
-$PGO_CMD $NS delete secret \
+$PGO_CMD delete secret -n $NS \
 	fromcrd-postgresuser-secret \
 	fromcrd-primaryuser-secret \
 	fromcrd-testuser-secret	> /dev/null
-$PGO_CMD $NS delete pgcluster fromcrd
-$PGO_CMD $NS delete pvc fromcrd
+$PGO_CMD delete pgcluster fromcrd -n $NS
+$PGO_CMD delete pvc fromcrd -n $NS
 
 # create the required postgres credentials for the fromcrd cluster
-$PGO_CMD $NS create -f $DIR/postgres-secret.yaml
-$PGO_CMD $NS create -f $DIR/primaryuser-secret.yaml
-$PGO_CMD $NS create -f $DIR/testuser-secret.yaml
+$PGO_CMD -n $NS create -f $DIR/postgres-secret.yaml
+$PGO_CMD -n $NS create -f $DIR/primaryuser-secret.yaml
+$PGO_CMD -n $NS create -f $DIR/testuser-secret.yaml
 
 # create the pgcluster CRD for the fromcrd cluster
-$PGO_CMD $NS create -f $DIR/fromcrd.json
+$PGO_CMD -n $NS create -f $DIR/fromcrd.json
 
