ebeacdb3303512298de86a89de95d81167712307
jmccormick2001 <jeff.mccormick@crunchydata.com>
2019-04-29 23:53:58 -0400

    add olm docs

---
+++ examples/olm/envs.sh
@@ -0,0 +1,44 @@
+export GOPATH=$HOME/odev
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+
+# NAMESPACE is the list of namespaces the Operator will watch
+export NAMESPACE=pgouser1,pgouser2
+
+# PGO_OPERATOR_NAMESPACE is the namespace the Operator is deployed into
+export PGO_OPERATOR_NAMESPACE=pgo
+
+# PGO_CMD values are either kubectl or oc, use oc if Openshift
+export PGO_CMD=oc
+
+# the directory location of the Operator scripts
+export PGOROOT=$GOPATH/src/github.com/crunchydata/postgres-operator
+
+# the version of the Operator you run is set by these vars
+export PGO_IMAGE_PREFIX=registry.connect.redhat.com/crunchydata
+export PGO_BASEOS=rhel7
+export PGO_VERSION=4.0.0-rc8
+export PGO_IMAGE_TAG=$PGO_BASEOS-$PGO_VERSION
+
+# for the pgo CLI to authenticate with using TLS
+#export PGO_CA_CERT=$PGOROOT/conf/postgres-operator/server.crt
+#export PGO_CLIENT_CERT=$PGOROOT/conf/postgres-operator/server.crt
+#export PGO_CLIENT_KEY=$PGOROOT/conf/postgres-operator/server.key
+
+# common bash functions for working with the Operator
+setip() 
+{ 
+	export PGO_APISERVER_URL=https://`$PGO_CMD -n "$PGO_OPERATOR_NAMESPACE" get service postgres-operator -o=jsonpath="{.spec.clusterIP}"`:8443 
+}
+
+alog() {
+$PGO_CMD  -n "$PGO_OPERATOR_NAMESPACE" logs `$PGO_CMD  -n "$PGO_OPERATOR_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c apiserver
+}
+
+olog () {
+$PGO_CMD  -n "$PGO_OPERATOR_NAMESPACE" logs `$PGO_CMD  -n "$PGO_OPERATOR_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c operator
+}
+
+slog () {
+$PGO_CMD  -n "$PGO_OPERATOR_NAMESPACE" logs `$PGO_CMD  -n "$PGO_OPERATOR_NAMESPACE" get pod --selector=name=postgres-operator -o jsonpath="{.items[0].metadata.name}"` -c scheduler
+}
