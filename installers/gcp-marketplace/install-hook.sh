eb1bd5206a459e4377b949cb0afed3c0bcfba451
Chris Bandy <chris.bandy@crunchydata.com>
2019-11-08 18:22:41 -0600

    GCP Marketplace installer
    Google Cloud Platform has a Marketplace for Kubernetes Applications that
    makes it easy for users to install and manage functionality.
    
    Issue: [ch6131]

---
+++ installers/gcp-marketplace/install-hook.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# vim: set noexpandtab :
+set -eu
+
+kc() { kubectl --namespace="$NAMESPACE" "$@"; }
+
+application_metadata="$( kc get "applications.app.k8s.io/$NAME" --output=json )"
+application_metadata="$( jq <<< "$application_metadata" '{ metadata: {
+	labels: { "app.kubernetes.io/name": .metadata.name },
+	ownerReferences: [{
+		apiVersion, kind, name: .metadata.name, uid: .metadata.uid
+	}]
+} }' )"
+
+existing="$( kc get deployment/postgres-operator --output=json 2> /dev/null || true )"
+
+if [ -n "$existing" ]; then
+	>&2 echo ERROR: Crunchy PostgreSQL Operator is already installed in this namespace
+	exit 1
+fi
+
+installer="$( /bin/config_env.py envsubst < /opt/postgres-operator/install-job.yaml )"
+inventory="$( /bin/config_env.py envsubst < /opt/postgres-operator/inventory.ini )"
+
+kc create --filename=/dev/stdin <<< "$installer"
+kc patch job/install-postgres-operator --type=strategic --patch="$application_metadata"
+
+job_metadata="$( kc get job/install-postgres-operator --output=json )"
+job_metadata="$( jq <<< "$job_metadata" '{ metadata: {
+	labels: { "app.kubernetes.io/name": .metadata.labels["app.kubernetes.io/name"] },
+	ownerReferences: [{
+		apiVersion, kind, name: .metadata.name, uid: .metadata.uid
+	}]
+} }' )"
+
+kc create secret generic install-postgres-operator --from-file=inventory=/dev/stdin <<< "$inventory"
+kc patch secret/install-postgres-operator --type=strategic --patch="$job_metadata"
+
+# Wait for either status condition then terminate the other.
+kc wait --for=condition=complete --timeout=5m job/install-postgres-operator &
+kc wait --for=condition=failed --timeout=5m job/install-postgres-operator &
+wait -n
+kill -s INT %% 2> /dev/null || true
+
+kc logs --selector=job-name=install-postgres-operator --tail=-1
+test 'Complete' = "$( kc get job/install-postgres-operator --output=jsonpath='{.status.conditions[*].type}' )"
+
+exec /opt/postgres-operator/cloud-marketplace-tools/bin/create_manifests.sh "$@"
