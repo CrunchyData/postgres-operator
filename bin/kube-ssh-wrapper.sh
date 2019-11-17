9660944666a24b6a21d2ccd36cdf4a637c11e67c
Andrew L'Ecuyer <andrew.lecuyer@crunchydata.com>
2019-11-17 23:30:26 +0000

    Updated the kube-ssh-wrapper.sh script to ensure the pod discovered for executing a backrest job (e.g. the pgBackRest dedicated repository host that must be discovered by the pgo backRest restore job when performing a restore) is running and not terminating.  This ensures that the proper pod is targeted in the event that a new pod is ready for use, but the old one is still terminating.  For instance, during a pgBackRest restore, the pgBackRest dedicated repository host is patched in order to update its configuration for the restored deployment.  This results in a period in which the old dedicated repository host might still be terminating, while the new repository host is ready for use.  This change ensures that only the running repo pod is considered by the restore job when it is run, therefore preventing the restore job from attempting to communicate with the terminating pod.
    Also streamlined the mechanism for parsing the pgBackRest
    options when executing a command via SSH.  This includes parsing
    the pgBackRest SSH command on 'pgbackrest' in order to cleanly
    parse out the SSH options and retrieve the pgBackRest command
    only (which is then utilized by kubectl).  Additionally, added
    additional comments to detail various steps in the script.

--- bin/kube-ssh-wrapper.sh
+++ bin/kube-ssh-wrapper.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# Determine if connecting to the backrest repo or primary db
+# Determine if connecting to the pgBackRest dedicated repository host or the primary database.  Specifically,
+# set the proper label to target one or the other when executing the kubectl command in place of SSH.
 if [[ "${PGO_BACKREST_REPO}" == "true" ]]
 then
+    # Add required env vars to environment. Specifically sets the CLUSTER_NAME when running on a dedicated
+    # pgBakcRest reposotiory host, as required to set variable CLUSTER_LABEL label below
     source "/tmp/pod_env.sh"
     TARGET_LABEL="role=master"
 else
     TARGET_LABEL="pgo-backrest-repo=true"
 fi
 
+# Set the name of the cluster the pgBackRest command is being executed for, as well as the name of the
+# container being targeted for the command
 CLUSTER_LABEL="pg-cluster=${CLUSTER_NAME}"
 CONTAINER_NAME="database"
 
-opts=$(echo "$@" | grep -o "\-\-c.*")
-pod=$(kubectl get pods --selector=${CLUSTER_LABEL},${TARGET_LABEL} --field-selector=status.phase=Running -o name)
+# Drop any SSH options to obtain the pgBackRest command only
+backrest_cmd=$(sed -n "s/^.*\(pgbackrest \)/\1/p" <<< "$@")
 
-exec kubectl exec -i "${pod}" -c ${CONTAINER_NAME} -- bash -c "pgbackrest ${opts}"
+# Find the pod to exec into using the labels defined above, and ensure the pod is running.  There might
+# still be a terminating pod with the same labels in certain situations, e.g. when an old pgBackRest repo 
+# pod is still terminating, and a new pgBackRest repo pod is up and running during a pgBackRest restore
+pod=$(kubectl get pods --selector="${CLUSTER_LABEL}","${TARGET_LABEL}" --no-headers | awk '/Running/ {print $1}')
+
+# Execute the kubectl command in place of SSH
+exec kubectl exec -i "${pod}" -c "${CONTAINER_NAME}" -- bash -c "${backrest_cmd}"
