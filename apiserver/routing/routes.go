390f2030a1a458eaf62db55299db9181074110f3
James Southard <james.southard@crunchydata.com>
2019-12-01 21:14:12 -0500

    breaks out service route registration for testing
    For one of my test scenarios, I broke out the version routes
    so that I could add only those to the router. It felt
    incomplete to only break out the one service...
    
    There's a reasonable argument to be made for pushing
    route registration into the service packages themselves,
    but I refrain from doing so as the refactoring of these
    apiserver packages may go in a wildly different direction

--- apiserver/routing/routes.go
+++ apiserver/routing/routes.go
@@ -49,32 +49,101 @@ import (
 // RegisterAllRoutes adds all routes supported by the apiserver to the
 // provided router
 func RegisterAllRoutes(r *mux.Router) {
+	RegisterVersionSvcRoutes(r)
+	RegisterPolicySvcRoutes(r)
+	RegisterWorkflowSvcRoutes(r)
+	RegisterPVCSvcRoutes(r)
+	RegisterPGOUserSvcRoutes(r)
+	RegisterPGORoleSvcRoutes(r)
+	RegisterLabelSvcRoutes(r)
+	RegisterLoadSvcRoutes(r)
+	RegisterUserSvcRoutes(r)
+	RegisterUpgradeSvcRoutes(r)
+	RegisterClusterSvcRoutes(r)
+	RegisterStatusSvcRoutes(r)
+	RegisterDfSvcRoutes(r)
+	RegisterConfigSvcRoutes(r)
+	RegisterNamespaceSvcRoutes(r)
+	RegisterBackupSvcRoutes(r)
+	RegisterBackrestSvcRoutes(r)
+	RegisterPGDumpSvcRoutes(r)
+	RegisterReloadSvcRoutes(r)
+	RegisterLsSvcRoutes(r)
+	RegisterCatSvcRoutes(r)
+	RegisterFailoverSvcRoutes(r)
+	RegisterPGBouncerSvcRoutes(r)
+	RegisterPGPoolSvcRoutes(r)
+	RegisterScheduleSvcRoutes(r)
+	RegisterBenchmarkSvcRoutes(r)
+
+}
+
+// RegisterVersionSvcRoutes registers all routes from the Version Service
+func RegisterVersionSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/version", versionservice.VersionHandler)
 	r.HandleFunc("/health", versionservice.HealthHandler)
+}
+
+// RegisterPolicySvcRoutes registers all routes from the Policy Service
+func RegisterPolicySvcRoutes(r *mux.Router) {
 	r.HandleFunc("/policies", policyservice.CreatePolicyHandler)
 	r.HandleFunc("/showpolicies", policyservice.ShowPolicyHandler).Methods("POST")
 	r.HandleFunc("/policiesdelete", policyservice.DeletePolicyHandler).Methods("POST")
+	r.HandleFunc("/policies/apply", policyservice.ApplyPolicyHandler).Methods("POST")
+}
+
+// RegisterWorkflowSvcRoutes registers all routes from the Workflow Service
+func RegisterWorkflowSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/workflow/{id}", workflowservice.ShowWorkflowHandler).Methods("GET")
+}
+
+// RegisterPVCSvcRoutes registers all routes from the PVC Service
+func RegisterPVCSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/showpvc", pvcservice.ShowPVCHandler).Methods("POST")
+}
+
+// RegisterPGOUserSvcRoutes registers all routes from the PGOUser Service
+func RegisterPGOUserSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/pgouserupdate", pgouserservice.UpdatePgouserHandler).Methods("POST")
 	r.HandleFunc("/pgouserdelete", pgouserservice.DeletePgouserHandler).Methods("POST")
 	r.HandleFunc("/pgousercreate", pgouserservice.CreatePgouserHandler).Methods("POST")
 	r.HandleFunc("/pgousershow", pgouserservice.ShowPgouserHandler).Methods("POST")
+}
+
+// RegisterPGORoleSvcRoutes registers all routes from the PGORole Service
+func RegisterPGORoleSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/pgoroleupdate", pgoroleservice.UpdatePgoroleHandler).Methods("POST")
 	r.HandleFunc("/pgoroledelete", pgoroleservice.DeletePgoroleHandler).Methods("POST")
 	r.HandleFunc("/pgorolecreate", pgoroleservice.CreatePgoroleHandler).Methods("POST")
 	r.HandleFunc("/pgoroleshow", pgoroleservice.ShowPgoroleHandler).Methods("POST")
-	r.HandleFunc("/policies/apply", policyservice.ApplyPolicyHandler).Methods("POST")
+}
+
+// RegisterLabelSvcRoutes registers all routes from the Label Service
+func RegisterLabelSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/label", labelservice.LabelHandler).Methods("POST")
 	r.HandleFunc("/labeldelete", labelservice.DeleteLabelHandler).Methods("POST")
-	r.HandleFunc("/load", loadservice.LoadHandler).Methods("POST")
+}
 
+// RegisterLoadSvcRoutes registers all routes from the Load Service
+func RegisterLoadSvcRoutes(r *mux.Router) {
+	r.HandleFunc("/load", loadservice.LoadHandler).Methods("POST")
+}
+
+// RegisterUserSvcRoutes registers all routes from the User Service
+func RegisterUserSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/userupdate", userservice.UpdateUserHandler).Methods("POST")
 	r.HandleFunc("/usercreate", userservice.CreateUserHandler).Methods("POST")
 	r.HandleFunc("/usershow", userservice.ShowUserHandler).Methods("POST")
 	r.HandleFunc("/userdelete", userservice.DeleteUserHandler).Methods("POST")
+}
 
+// RegisterUpgradeSvcRoutes registers all routes from the Upgrade Service
+func RegisterUpgradeSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/upgrades", upgradeservice.CreateUpgradeHandler).Methods("POST")
+}
+
+// RegisterClusterSvcRoutes registers all routes from the Cluster Service
+func RegisterClusterSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/clusters", clusterservice.CreateClusterHandler).Methods("POST")
 	r.HandleFunc("/showclusters", clusterservice.ShowClusterHandler).Methods("POST")
 	r.HandleFunc("/clustersdelete", clusterservice.DeleteClusterHandler).Methods("POST")
@@ -83,43 +152,90 @@ func RegisterAllRoutes(r *mux.Router) {
 	r.HandleFunc("/clusters/scale/{name}", clusterservice.ScaleClusterHandler)
 	r.HandleFunc("/scale/{name}", clusterservice.ScaleQueryHandler).Methods("GET")
 	r.HandleFunc("/scaledown/{name}", clusterservice.ScaleDownHandler).Methods("GET")
+}
+
+// RegisterStatusSvcRoutes registers all routes from the Status Service
+func RegisterStatusSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/status", statusservice.StatusHandler)
+}
+
+// RegisterDfSvcRoutes registers all routes from the Df Service
+func RegisterDfSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/df/{name}", dfservice.DfHandler)
+}
+
+// RegisterConfigSvcRoutes registers all routes from the Config Service
+func RegisterConfigSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/config", configservice.ShowConfigHandler)
+}
+
+// RegisterNamespaceSvcRoutes registers all routes from the Namespace Service
+func RegisterNamespaceSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/namespace", namespaceservice.ShowNamespaceHandler).Methods("POST")
 	r.HandleFunc("/namespacedelete", namespaceservice.DeleteNamespaceHandler).Methods("POST")
 	r.HandleFunc("/namespacecreate", namespaceservice.CreateNamespaceHandler).Methods("POST")
 	r.HandleFunc("/namespaceupdate", namespaceservice.UpdateNamespaceHandler).Methods("POST")
+}
 
-	// backups / backrest
+// RegisterBackupSvcRoutes registers all routes from the Backup Service
+func RegisterBackupSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/backups/{name}", backupservice.ShowBackupHandler).Methods("GET")
 	r.HandleFunc("/backupsdelete/{name}", backupservice.DeleteBackupHandler).Methods("GET")
 	r.HandleFunc("/backups", backupservice.CreateBackupHandler).Methods("POST")
 	r.HandleFunc("/pgbasebackuprestore", backupservice.RestoreHandler).Methods("POST")
+}
+
+// RegisterBackrestSvcRoutes registers all routes from the Backrest Service
+func RegisterBackrestSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/backrestbackup", backrestservice.CreateBackupHandler).Methods("POST")
 	r.HandleFunc("/backrest/{name}", backrestservice.ShowBackrestHandler).Methods("GET")
 	r.HandleFunc("/restore", backrestservice.RestoreHandler).Methods("POST")
+}
 
-	// pgdump
+// RegisterPGDumpSvcRoutes registers all routes from the PGDump Service
+func RegisterPGDumpSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/pgdumpbackup", pgdumpservice.BackupHandler).Methods("POST")
 	r.HandleFunc("/pgdump/{name}", pgdumpservice.ShowDumpHandler).Methods("GET")
 	r.HandleFunc("/pgdumprestore", pgdumpservice.RestoreHandler).Methods("POST")
+}
 
+// RegisterReloadSvcRoutes registers all routes from the Reload Service
+func RegisterReloadSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/reload", reloadservice.ReloadHandler).Methods("POST")
+}
+
+// RegisterLsSvcRoutes registers all routes from the Ls Service
+func RegisterLsSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/ls", lsservice.LsHandler).Methods("POST")
+}
+
+// RegisterCatSvcRoutes registers all routes from the Cat Service
+func RegisterCatSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/cat", catservice.CatHandler).Methods("POST")
+}
+
+// RegisterFailoverSvcRoutes registers all routes from the Failover Service
+func RegisterFailoverSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/failover", failoverservice.CreateFailoverHandler).Methods("POST")
 	r.HandleFunc("/failover/{name}", failoverservice.QueryFailoverHandler).Methods("GET")
+}
+
+// RegisterPGBouncerSvcRoutes registers all routes from the PGBouncer Service
+func RegisterPGBouncerSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/pgbouncer", pgbouncerservice.CreatePgbouncerHandler).Methods("POST")
 	r.HandleFunc("/pgbouncer", pgbouncerservice.DeletePgbouncerHandler).Methods("DELETE")
 	r.HandleFunc("/pgbouncerdelete", pgbouncerservice.DeletePgbouncerHandler).Methods("POST")
+}
 
-	//schedule
+// RegisterScheduleSvcRoutes registers all routes from the Schedule Service
+func RegisterScheduleSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/schedule", scheduleservice.CreateScheduleHandler).Methods("POST")
 	r.HandleFunc("/scheduledelete", scheduleservice.DeleteScheduleHandler).Methods("POST")
 	r.HandleFunc("/scheduleshow", scheduleservice.ShowScheduleHandler).Methods("POST")
+}
 
-	//benchmark
+// RegisterBenchmarkSvcRoutes registers all routes from the Benchmark Service
+func RegisterBenchmarkSvcRoutes(r *mux.Router) {
 	r.HandleFunc("/benchmark", benchmarkservice.CreateBenchmarkHandler).Methods("POST")
 	r.HandleFunc("/benchmarkdelete", benchmarkservice.DeleteBenchmarkHandler).Methods("POST")
 	r.HandleFunc("/benchmarkshow", benchmarkservice.ShowBenchmarkHandler).Methods("POST")
