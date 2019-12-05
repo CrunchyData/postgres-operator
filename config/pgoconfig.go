702b10fc9f4d298b0c5231826bafd12213d385ab
Andrew L'Ecuyer <andrew.lecuyer@crunchydata.com>
2019-12-05 22:48:23 +0000

    Fix indentation for proper formatting.

--- config/pgoconfig.go
+++ config/pgoconfig.go
@@ -222,7 +222,7 @@ type ClusterStruct struct {
 	DisableAutofail         bool   `yaml:"DisableAutofail"`
 	AutofailReplaceReplica  bool   `yaml:"AutofailReplaceReplica"`
 	PgmonitorPassword       string `yaml:"PgmonitorPassword"`
-	EnableCrunchyadm		bool   `yaml:"EnableCrunchyadm"`
+	EnableCrunchyadm        bool   `yaml:"EnableCrunchyadm"`
 }
 
 type StorageStruct struct {
