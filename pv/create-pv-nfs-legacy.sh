003810f40fbef91186a8e03e6bcc005722eebadb
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2020-01-25 18:08:28 -0500

    Update NFS PV creation script to mimic "production" environments
    The legacy NFS PV (persistent volume) creation script would expect
    all of the PVs to exist in the same directory on the NFS mount point.
    While this provides certain conveniences when developing and debugging
    the Operator, it does not necessarily mimic production environments, and
    could lead to creating some weird situations when trying to plan out
    new features. Additionally, most PV provisioners do not do this as well,
    for convenience and security as well.
    
    This updates the "create-pv-nfs.sh" script to create a directory for
    each PV that is created on the NFS filesystem. The script is very
    opinionated (and could use a few more guardrails) and is still not
    recommend for production use.
    
    The legacy version of the script has been renamed to
    "create-pv-nfs-legacy.sh" and is still available should one want to use
    it. Likely, this will be removed in the future as we remove reliance on
    "expenv"

---
+++ pv/create-pv-nfs-legacy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2017 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "create the test PV and PVC using the NFS dir"
+for i in {1..160}
+do
+   	echo "creating PV crunchy-pv$i"
+	export COUNTER=$i
+	$PGO_CMD --namespace=$PGO_OPERATOR_NAMESPACE delete pv crunchy-pv$i
+	expenv -f $DIR/crunchy-pv-nfs.json | $PGO_CMD --namespace=$PGO_OPERATOR_NAMESPACE create -f -
+done
