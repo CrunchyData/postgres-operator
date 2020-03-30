CrunchyData/postgres-operator

commit 1e473a86e289a5a8836ebe3b9213a898f54cda07

Enable Standby Cluster Creation with S3 Storage
This commit enables standby cluster creation, specifically for those
using AWS S3 storage.  AWS S3 provides a central pgBackRest repository
that can accessed by multiple clusters living in separate Kubernetes
clusters.  This therefore allows the PG cluster that is currently
accepting writes to push WAL to a central repo (S3), where it can then
be obtained and replayed by one or more standby clusters.  Being that
AWS S3 is utilized as part of this architecture, no direct
communication between clusters is required.  However, please note that
this does mean that streaming replication is not currently supported
for standby clusters.

In support of this feature, two new flags have been added to the
"pgo create cluster" command, which are as follows:

- standby: boolean that determines whether or not the cluster should be
created as a standby cluster
- backrest-repo-path: Allows the user to override the pgBackRest
repository path for a cluster.  While this setting can now be utilized
when creating any cluster, it is typically required for the creation
of standby clusters, since the repo path setting will need to match
that of a remote/external PG database, and will therefore likely need
to be changed from the current default.

The flags referenced above have corresponding parameters in the
in both the apiserver and operator API's, therefore allowing standby
clusters to be created whether using the pgo client, apiserver or if
submitting pgcluster CR's directly (ensuring the proper S3
configuration is provided as needed for pgBackRest S3 support).

When creating standby clusters, the lifecycle for cluster
initialization is modified.  Specifically, since the the S3 repo
already exists and is being managed by a separate PG cluster/database,
no stanza creation is needed in the S3 repo.  However, if local and
S3 storage are selected for the standby cluster, then a stanza will

operator/cluster/clone.go
@@ -42,7 +42,6 @@ import (
 )
 
 const (
-	pgBackRestRepoPath                   = "/backrestrepo/" + backrest.BackrestRepoServiceName
 	pgBackRestRepoSyncContainerImageName = "%s/pgo-backrest-repo-sync:%s"
 	pgBackRestRepoSyncJobNamePrefix      = "pgo-backrest-repo-sync-%s-%s"
 	pgBackRestStanza                     = "db" // this is hardcoded throughout...
@@ -370,7 +369,7 @@ func cloneStep2(clientset *kubernetes.Clientset, client *rest.RESTClient, namesp
 		PGOImageTag:         operator.Pgo.Pgo.PGOImageTag,
 		PgbackrestStanza:    pgBackRestStanza,
 		PgbackrestDBPath:    fmt.Sprintf(targetClusterPGDATAPath, targetClusterName),
-		PgbackrestRepo1Path: fmt.Sprintf(pgBackRestRepoPath, targetClusterName),
+		PgbackrestRepo1Path: util.GetPGBackRestRepoPath(sourcePgcluster),
 		PgbackrestRepo1Host: fmt.Sprintf(backrest.BackrestRepoServiceName, targetClusterName),
 		PgbackrestRepoType:  operator.GetRepoType(task.Spec.Parameters["backrestStorageType"]),
 		PgbackrestS3EnvVars: operator.GetPgbackrestS3EnvVars(sourcePgcluster, clientset, namespace),
@@ -557,13 +556,17 @@ func createPgBackRestRepoSyncJob(clientset *kubernetes.Clientset, namespace stri
 								},
 								v1.EnvVar{
 									Name:  "PGBACKREST_REPO1_PATH",
-									Value: fmt.Sprintf(pgBackRestRepoPath, sourcePgcluster.Spec.ClusterName),
+									Value: util.GetPGBackRestRepoPath(sourcePgcluster),
 								},
 								// NOTE: this needs to be a name like this in order to not
 								// confuse pgBackRest, which does support "REPO*" name
 								v1.EnvVar{
-									Name:  "NEW_PGBACKREST_REPO",
-									Value: fmt.Sprintf(pgBackRestRepoPath, targetClusterName),
+									Name: "NEW_PGBACKREST_REPO",
+									Value: util.GetPGBackRestRepoPath(crv1.Pgcluster{
+										ObjectMeta: meta_v1.ObjectMeta{
+											Name: targetClusterName,
+										},
+									}),
 								},
 							},
 							VolumeMounts: []v1.VolumeMount{

apis/cr/v1/cluster.go
@@ -71,9 +71,11 @@ type PgclusterSpec struct {
 	BackrestS3Bucket   string                   `json:"backrestS3Bucket"`
 	BackrestS3Region   string                   `json:"backrestS3Region"`
 	BackrestS3Endpoint string                   `json:"backrestS3Endpoint"`
+	BackrestRepoPath   string                   `json:"backrestRepoPath"`
 	TablespaceMounts   map[string]PgStorageSpec `json:"tablespaceMounts"`
 	TLS                TLSSpec                  `json:"tls"`
 	TLSOnly            bool                     `json:"tlsOnly"`
+	Standby            bool                     `json:"standby"`
 }
 
 // PgclusterList is the CRD that defines a Crunchy PG Cluster List

apiserver/clusterservice/clusterimpl.go
@@ -617,6 +617,16 @@ func CreateCluster(request *msgs.CreateClusterRequest, ns, pgouser string) msgs.
 		userLabelsMap[config.LABEL_SERVICE_TYPE] = request.ServiceType
 	}
 
+	// if the request is for a standby cluster then validate it to ensure all parameters have
+	// been properly specifed as required to create a standby cluster
+	if request.Standby {
+		if err := validateStandbyCluster(request); err != nil {
+			resp.Status.Code = msgs.Error
+			resp.Status.Msg = err.Error()
+			return resp
+		}
+	}
+
 	// ensure the backrest storage type specified for the cluster is valid, and that the
 	// configruation required to use that storage type (e.g. a bucket, endpoint and region
 	// when using aws s3 storage) has been provided
@@ -1119,6 +1129,11 @@ func getClusterParams(request *msgs.CreateClusterRequest, name string, userLabel
 		labels[config.LABEL_AUTOFAIL] = "true"
 	}
 
+	// set whether or not the cluster will be a standby cluster
+	spec.Standby = request.Standby
+	// set the pgBackRest repository path
+	spec.BackrestRepoPath = request.BackrestRepoPath
+
 	//pgbadger - set with global flag first then check for a user flag
 	labels[config.LABEL_BADGER] = strconv.FormatBool(apiserver.BadgerFlag)
 	if request.BadgerFlag {
@@ -1569,4 +1584,16 @@ func isMissingS3Config(request *msgs.CreateClusterRequest) bool {
 		return true
 	}
 	return false
+}
+
+func validateStandbyCluster(request *msgs.CreateClusterRequest) error {
+	switch {
+	case !strings.Contains(request.BackrestStorageType, "s3"):
+		return errors.New("Backrest storage type 's3' must be selected in order to create a " +
+			"standby cluster")
+	case request.BackrestRepoPath == "":
+		return errors.New("A pgBackRest repository path must be specified when creating a " +
+			"standby cluster")
+	}
+	return nil
 }

apiservermsgs/clustermsgs.go
@@ -82,6 +82,8 @@ type CreateClusterRequest struct {
 	BackrestS3Bucket          string
 	BackrestS3Region          string
 	BackrestS3Endpoint        string
+	Standby                   bool
+	BackrestRepoPath          string
 	// allow the user to set custom sizes for PVCs
 	// PVCSize applies to the primary/replica storage specs
 	PVCSize string

bin/pgo-backrest-repo/pgo-backrest-repo.sh
@@ -32,9 +32,9 @@ ls $CONFIG
 echo "REPO is ..."
 ls $REPO
 
-if [ ! -d $PGBACKREST_REPO_PATH ]; then
-	echo "creating " $PGBACKREST_REPO_PATH
-	mkdir -p $PGBACKREST_REPO_PATH
+if [ ! -d $PGBACKREST_REPO1_PATH ]; then
+	echo "creating " $PGBACKREST_REPO1_PATH
+	mkdir -p $PGBACKREST_REPO1_PATH
 fi
 
 # This is a workaround for changes introduced in pgBackRest v2.24.  Specifically, a pg1-path

controller/job/backresthandler.go
@@ -21,6 +21,7 @@ import (
 
 	crv1 "github.com/crunchydata/postgres-operator/apis/cr/v1"
 	"github.com/crunchydata/postgres-operator/config"
+	"github.com/crunchydata/postgres-operator/controller"
 	"github.com/crunchydata/postgres-operator/events"
 	"github.com/crunchydata/postgres-operator/kubeapi"
 	backrestoperator "github.com/crunchydata/postgres-operator/operator/backrest"
@@ -154,49 +155,19 @@ func (c *Controller) handleBackrestBackupUpdate(job *apiv1.Job) error {
 	}
 	publishBackupComplete(labels[config.LABEL_PG_CLUSTER], job.ObjectMeta.Labels[config.LABEL_PG_CLUSTER_IDENTIFIER], job.ObjectMeta.Labels[config.LABEL_PGOUSER], "pgbackrest", job.ObjectMeta.Namespace, "")
 
-	// if initial cluster backup, now annotate all existing pgreplica's to initiate replica creation
-	pgreplicaList := &crv1.PgreplicaList{}
-	selector := config.LABEL_PG_CLUSTER + "=" + labels[config.LABEL_PG_CLUSTER]
+	// If the completed backup was a cluster bootstrap backup, then mark the cluster as initialized
+	// and initiate the creation of any replicas.  Otherwise if the completed backup was taken as
+	// the result of a failover, then proceed with tremove the "primary_on_role_change" tag.
 	if labels[config.LABEL_PGHA_BACKUP_TYPE] == crv1.BackupTypeBootstrap {
 		log.Debugf("jobController onUpdate initial backup complete")
 
-		// get the pgcluster resource for the cluster the replica is a part of
-		cluster := crv1.Pgcluster{}
-		_, err = kubeapi.Getpgcluster(c.JobClient, &cluster, labels[config.LABEL_PG_CLUSTER],
+		controller.SetClusterInitializedStatus(c.JobClient, labels[config.LABEL_PG_CLUSTER],
 			job.ObjectMeta.Namespace)
-		if err != nil {
-			log.Error(err)
-			return err
-		}
-		clusterStatus := cluster.Status.State
-		message := "Cluster has been initialized"
-		err = kubeapi.PatchpgclusterStatus(c.JobClient, crv1.PgclusterStateInitialized, message,
-			&cluster, job.ObjectMeta.Namespace)
-		if err != nil {
-			log.Error(err)
-			return err
-		}
 
-		err := kubeapi.GetpgreplicasBySelector(c.JobClient, pgreplicaList, selector, job.ObjectMeta.Namespace)
-		if err != nil {
-			log.Error(err)
-			return err
-		}
-		for _, pgreplica := range pgreplicaList.Items {
-			if pgreplica.Annotations == nil {
-				pgreplica.Annotations = make(map[string]string)
-			}
-			if clusterStatus == crv1.PgclusterStateRestore {
-				pgreplica.Spec.Status = "restore"
-			} else {
-				pgreplica.Annotations[config.ANNOTATION_PGHA_BOOTSTRAP_REPLICA] = "true"
-			}
-			err = kubeapi.Updatepgreplica(c.JobClient, &pgreplica, pgreplica.Name, job.ObjectMeta.Namespace)
-			if err != nil {
-				log.Error(err)
-				return err
-			}
-		}
+		// now initialize the creation of any replica
+		controller.InitializeReplicaCreation(c.JobClient, labels[config.LABEL_PG_CLUSTER],
+			job.ObjectMeta.Namespace)
+
 	} else if labels[config.LABEL_PGHA_BACKUP_TYPE] == crv1.BackupTypeFailover {
 		err := clusteroperator.RemovePrimaryOnRoleChangeTag(c.JobClientset, c.JobConfig,
 			labels[config.LABEL_PG_CLUSTER], job.ObjectMeta.Namespace)
@@ -213,10 +184,15 @@ func (c *Controller) handleBackrestStanzaCreateUpdate(job *apiv1.Job) error {
 
 	labels := job.GetObjectMeta().GetLabels()
 	log.Debugf("jobController onUpdate backrest stanza-create job case")
+
+	// grab the cluster name and namespace for use in various places below
+	clusterName := labels[config.LABEL_PG_CLUSTER]
+	namespace := job.Namespace
+
 	if job.Status.Succeeded == 1 {
-		log.Debugf("backrest stanza successfully created for cluster %s", labels[config.LABEL_PG_CLUSTER])
+		log.Debugf("backrest stanza successfully created for cluster %s", clusterName)
 		log.Debugf("proceeding with the initial full backup for cluster %s as needed for replica creation",
-			labels[config.LABEL_PG_CLUSTER])
+			clusterName)
 
 		var backrestRepoPodName string
 		for _, cont := range job.Spec.Template.Spec.Containers {
@@ -224,12 +200,28 @@ func (c *Controller) handleBackrestStanzaCreateUpdate(job *apiv1.Job) error {
 				if envVar.Name == "PODNAME" {
 					backrestRepoPodName = envVar.Value
 					log.Debugf("the backrest repo pod for the initial backup of cluster %s is %s",
-						labels[config.LABEL_PG_CLUSTER], backrestRepoPodName)
+						clusterName, backrestRepoPodName)
 				}
 			}
 		}
+
+		cluster := crv1.Pgcluster{}
+		if _, err := kubeapi.Getpgcluster(c.JobClient, &cluster,
+			clusterName, namespace); err != nil {
+			return err
+		}
+		// If the cluster is a standby cluster, then no need to proceed with backup creation.
+		// Instead the cluster can be set to initialized following creation of the stanza.
+		if cluster.Spec.Standby {
+			log.Debugf("job Controller: standby cluster %s will now be set to an initialized "+
+				"status", clusterName)
+			controller.SetClusterInitializedStatus(c.JobClient, clusterName, namespace)
+			return nil
+		}
+
 		backrestoperator.CreateInitialBackup(c.JobClient, job.ObjectMeta.Namespace,
-			labels[config.LABEL_PG_CLUSTER], backrestRepoPodName)
+			clusterName, backrestRepoPodName)
+
 	}
 	return nil
 }

controller/pod/podcontroller.go
@@ -267,12 +267,28 @@ func (c *Controller) checkReadyStatus(oldpod, newpod *apiv1.Pod, cluster *crv1.P
 					publishClusterComplete(clusterName, newpod.ObjectMeta.Namespace, cluster)
 					//
 
-					if cluster.Labels[config.LABEL_BACKREST] == "true" {
-						tmptask := crv1.Pgtask{}
-						found, err := kubeapi.Getpgtask(c.PodClient, &tmptask, clusterName+"-stanza-create", newpod.ObjectMeta.Namespace)
-						if !found && err != nil {
-							backrestoperator.StanzaCreate(newpod.ObjectMeta.Namespace, clusterName, c.PodClientset, c.PodClient)
-						}
+					// Proceed with stanza-creation of this is not a standby cluster, or if its
+					// a standby cluster that does not have "s3" storage only enabled.
+					// If this is a standby cluster and the pgBackRest storage type is set
+					// to "s3" for S3 storage only, set the cluster to an initialized status.
+					if !cluster.Spec.Standby || (cluster.Spec.Standby &&
+						cluster.Spec.UserLabels[config.LABEL_BACKREST_STORAGE_TYPE] != "s3") {
+						backrestoperator.StanzaCreate(newpod.ObjectMeta.Namespace, clusterName,
+							c.PodClientset, c.PodClient)
+					} else {
+						controller.SetClusterInitializedStatus(c.PodClient, clusterName,
+							newpod.ObjectMeta.Namespace)
+					}
+
+					// If a standby cluster initialize the creation of any replicas.  Replicas
+					// can be initialized right away, i.e. there is no dependency on
+					// stanza-creation and/or the creation of any backups, since the replicas
+					// will be generated from the pgBackRest repository of an external PostgreSQL
+					// database (which should already exist).
+					if cluster.Spec.Standby {
+						controller.InitializeReplicaCreation(c.PodClient, clusterName,
+							newpod.ObjectMeta.Namespace)
+						return
 					}
 
 					// if this is a pgbouncer enabled cluster, add a pgbouncer

operator/backrest/repo.go
@@ -107,7 +107,7 @@ func CreateRepoDeployment(clientset *kubernetes.Clientset, namespace string, clu
 		BackrestRepoClaimName: repoName,
 		SshdSecretsName:       "pgo-backrest-repo-config",
 		PGbackrestDBHost:      cluster.Name,
-		PgbackrestRepoPath:    "/backrestrepo/" + serviceName,
+		PgbackrestRepoPath:    util.GetPGBackRestRepoPath(*cluster),
 		PgbackrestDBPath:      "/pgdata/" + cluster.Name,
 		PgbackrestPGPort:      cluster.Spec.Port,
 		SshdPort:              operator.Pgo.Cluster.BackrestPort,

commit bbedc5d84b1d7554b81078d14588f3e81367d1e4

Ensure clone restores a cluster from the new pgBackRest repo
A regression introduced in 1e473a86e would cause the second step of
a cluster clone to fail, as it was attempting to reference the
pgBackRest repository of the source cluster, not the target. As such,
it was unable to find its manifest file and failed.

This switches the reference to be that of the target cluster.

Issue: [ch7748]

operator/cluster/clone.go
@@ -368,7 +368,7 @@ func cloneStep2(clientset *kubernetes.Clientset, client *rest.RESTClient, namesp
 		PGOImageTag:         operator.Pgo.Pgo.PGOImageTag,
 		PgbackrestStanza:    pgBackRestStanza,
 		PgbackrestDBPath:    fmt.Sprintf(targetClusterPGDATAPath, targetClusterName),
-		PgbackrestRepo1Path: util.GetPGBackRestRepoPath(sourcePgcluster),
+		PgbackrestRepo1Path: util.GetPGBackRestRepoPath(targetPgcluster),
 		PgbackrestRepo1Host: fmt.Sprintf(backrest.BackrestRepoServiceName, targetClusterName),
 		PgbackrestRepoType:  operator.GetRepoType(task.Spec.Parameters["backrestStorageType"]),
 		PgbackrestS3EnvVars: operator.GetPgbackrestS3EnvVars(sourcePgcluster, clientset, namespace),