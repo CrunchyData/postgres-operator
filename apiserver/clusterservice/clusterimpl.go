3eceeb6cc47ccf3622abeb5cd40759cb75654fa7
Andrew L'Ecuyer <andrew.lecuyer@crunchydata.com>
2020-03-31 16:13:33 +0000

    Delete Existing rmdata pgtask When Deleting Cluster
    When creating a new rmdata pgtask to delete a cluster, an attempt is
    first made to delete any pgtask for the cluster with the same name.
    This ensures any existing rmdata pgtasks with the same name are first
    removed prior to creating the new pgtask, preventing
    'pgtask already exists' errors when attempting to delete a cluster.

--- apiserver/clusterservice/clusterimpl.go
+++ apiserver/clusterservice/clusterimpl.go
@@ -86,6 +86,14 @@ func DeleteCluster(name, selector string, deleteData, deleteBackups bool, ns, pg
 		replicaName := ""
 		clusterPGHAScope := cluster.ObjectMeta.Labels[config.LABEL_PGHA_SCOPE]
 
+		// first delete any existing rmdata pgtask with the same name
+		if err := kubeapi.Deletepgtask(apiserver.RESTClient, taskName, ns); err != nil &&
+			!kerrors.IsNotFound(err) {
+			response.Status.Code = msgs.Error
+			response.Status.Msg = err.Error()
+			return response
+		}
+
 		err := apiserver.CreateRMDataTask(cluster.Spec.Name, replicaName, taskName, deleteBackups, deleteData, isReplica, isBackup, ns, clusterPGHAScope)
 		if err != nil {
 			log.Debugf("error on creating rmdata task %s", err.Error())
