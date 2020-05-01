b54e7d8a6685c1dad668a5a76440c74b7e9fd3a6
James Southard <james.southard@crunchydata.com>
2020-05-01 07:57:31 -0400

    adds backoff test cases for ensuring extents
    The new tests cover a case where there was no failure when the
    JitterMode was set to a range significantly smaller than the one under test

--- internal/pgadmin/backoff_test.go
+++ internal/pgadmin/backoff_test.go
@@ -22,7 +22,7 @@ import (
 
 type testPair struct {
 	Exp  time.Duration
-	Test int
+	Iter int
 }
 
 func TestDoubleExp(t *testing.T) {
@@ -31,20 +31,20 @@ func TestDoubleExp(t *testing.T) {
 		Ratio: 2,
 	}
 	cases := []testPair{
-		{Test: 0, Exp: 10 * time.Millisecond},
-		{Test: 1, Exp: 20 * time.Millisecond},
-		{Test: 2, Exp: 40 * time.Millisecond},
-		{Test: 3, Exp: 80 * time.Millisecond},
-		{Test: 4, Exp: 160 * time.Millisecond},
-		{Test: 5, Exp: 320 * time.Millisecond},
-		{Test: 6, Exp: 640 * time.Millisecond},
-		{Test: 7, Exp: 1280 * time.Millisecond},
-		{Test: 8, Exp: 2560 * time.Millisecond},
-		{Test: 9, Exp: 5120 * time.Millisecond},
+		{Iter: 0, Exp: 10 * time.Millisecond},
+		{Iter: 1, Exp: 20 * time.Millisecond},
+		{Iter: 2, Exp: 40 * time.Millisecond},
+		{Iter: 3, Exp: 80 * time.Millisecond},
+		{Iter: 4, Exp: 160 * time.Millisecond},
+		{Iter: 5, Exp: 320 * time.Millisecond},
+		{Iter: 6, Exp: 640 * time.Millisecond},
+		{Iter: 7, Exp: 1280 * time.Millisecond},
+		{Iter: 8, Exp: 2560 * time.Millisecond},
+		{Iter: 9, Exp: 5120 * time.Millisecond},
 	}
 
 	for _, tCase := range cases {
-		if res := bp.Duration(tCase.Test); res != tCase.Exp {
+		if res := bp.Duration(tCase.Iter); res != tCase.Exp {
 			t.Logf("Expected %v, Got %v", tCase.Exp, res)
 			t.Fail()
 		}
@@ -59,14 +59,14 @@ func TestCalcMax(t *testing.T) {
 		Maximum: limit,
 	}
 	cases := []testPair{
-		{Test: 6, Exp: 640 * time.Millisecond},
-		{Test: 7, Exp: limit},
-		{Test: 8, Exp: limit},
-		{Test: 9, Exp: limit},
+		{Iter: 6, Exp: 640 * time.Millisecond},
+		{Iter: 7, Exp: limit},
+		{Iter: 8, Exp: limit},
+		{Iter: 9, Exp: limit},
 	}
 
 	for _, tCase := range cases {
-		if res := bp.Duration(tCase.Test); res != tCase.Exp {
+		if res := bp.Duration(tCase.Iter); res != tCase.Exp {
 			t.Logf("Expected %v, Got %v", tCase.Exp, res)
 			t.Fail()
 		}
@@ -85,20 +85,20 @@ func TestUniformPolicy(t *testing.T) {
 	}
 
 	cases := []testPair{
-		{Test: 0, Exp: 8 * time.Second},
-		{Test: 1, Exp: 8 * time.Second},
-		{Test: 2, Exp: 8 * time.Second},
-		{Test: 3, Exp: 8 * time.Second},
-		{Test: 4, Exp: 8 * time.Second},
-		{Test: 5, Exp: 8 * time.Second},
-		{Test: 6, Exp: 8 * time.Second},
-		{Test: 7, Exp: 8 * time.Second},
-		{Test: 8, Exp: 8 * time.Second},
-		{Test: 9, Exp: 8 * time.Second},
+		{Iter: 0, Exp: 8 * time.Second},
+		{Iter: 1, Exp: 8 * time.Second},
+		{Iter: 2, Exp: 8 * time.Second},
+		{Iter: 3, Exp: 8 * time.Second},
+		{Iter: 4, Exp: 8 * time.Second},
+		{Iter: 5, Exp: 8 * time.Second},
+		{Iter: 6, Exp: 8 * time.Second},
+		{Iter: 7, Exp: 8 * time.Second},
+		{Iter: 8, Exp: 8 * time.Second},
+		{Iter: 9, Exp: 8 * time.Second},
 	}
 
 	for _, tCase := range cases {
-		if res := bp.Duration(tCase.Test); res != tCase.Exp {
+		if res := bp.Duration(tCase.Iter); res != tCase.Exp {
 			t.Logf("Expected %v, Got %v", tCase.Exp, res)
 			t.Fail()
 		}
@@ -122,27 +122,27 @@ func TestStatedPolicy(t *testing.T) {
 	}
 
 	cases := []testPair{
-		{Test: 0, Exp: 1 * time.Millisecond},
-		{Test: 1, Exp: 1 * time.Millisecond},
-		{Test: 2, Exp: 2 * time.Millisecond},
-		{Test: 3, Exp: 3 * time.Millisecond},
-		{Test: 4, Exp: 5 * time.Millisecond},
-		{Test: 5, Exp: 8 * time.Millisecond},
-		{Test: 6, Exp: 13 * time.Millisecond},
-		{Test: 7, Exp: 21 * time.Millisecond},
-		{Test: 8, Exp: 33 * time.Millisecond},
-		{Test: 9, Exp: 54 * time.Millisecond},
+		{Iter: 0, Exp: 1 * time.Millisecond},
+		{Iter: 1, Exp: 1 * time.Millisecond},
+		{Iter: 2, Exp: 2 * time.Millisecond},
+		{Iter: 3, Exp: 3 * time.Millisecond},
+		{Iter: 4, Exp: 5 * time.Millisecond},
+		{Iter: 5, Exp: 8 * time.Millisecond},
+		{Iter: 6, Exp: 13 * time.Millisecond},
+		{Iter: 7, Exp: 21 * time.Millisecond},
+		{Iter: 8, Exp: 33 * time.Millisecond},
+		{Iter: 9, Exp: 54 * time.Millisecond},
 	}
 
 	for _, tCase := range cases {
-		if res := bp.Duration(tCase.Test); res != tCase.Exp {
+		if res := bp.Duration(tCase.Iter); res != tCase.Exp {
 			t.Logf("Expected %v, Got %v", tCase.Exp, res)
 			t.Fail()
 		}
 	}
 }
 
-func TestJitterFull(t *testing.T) {
+func TestJitterFullLimits(t *testing.T) {
 	bp := SpecificBackoffPolicy{
 		Times: []time.Duration{
 			10 * time.Second,
@@ -157,7 +157,31 @@ func TestJitterFull(t *testing.T) {
 	}
 }
 
-func TestJitterCenter(t *testing.T) {
+func TestJitterFullExtents(t *testing.T) {
+	bp := SpecificBackoffPolicy{
+		Times: []time.Duration{
+			10 * time.Second,
+		},
+		JitterMode: JitterFull,
+	}
+
+	var nearLow, nearHigh bool
+	for i := 0; i < 1000; i++ {
+		// See if we've had at least one value near the low limit
+		if d := bp.Duration(i); !nearLow && d < 250*time.Millisecond {
+			nearLow = true
+		}
+		// See if we've had at least one value near the high limit
+		if d := bp.Duration(i); !nearHigh && d > 9750*time.Millisecond {
+			nearHigh = true
+		}
+	}
+	if !(nearLow && nearHigh) {
+		t.Fatalf("Expected generated values near edges: near low [%t], near high [%t]", nearLow, nearHigh)
+	}
+}
+
+func TestJitterCenterLimits(t *testing.T) {
 	bp := SpecificBackoffPolicy{
 		Times: []time.Duration{
 			20 * time.Second,
@@ -172,7 +196,31 @@ func TestJitterCenter(t *testing.T) {
 	}
 }
 
-func TestJitterSmall(t *testing.T) {
+func TestJitterCenterExtents(t *testing.T) {
+	bp := SpecificBackoffPolicy{
+		Times: []time.Duration{
+			20 * time.Second,
+		},
+		JitterMode: JitterCenter,
+	}
+
+	var nearLow, nearHigh bool
+	for i := 0; i < 1000; i++ {
+		// See if we've had at least one value near the low limit
+		if d := bp.Duration(i); !nearLow && d < 10250*time.Millisecond {
+			nearLow = true
+		}
+		// See if we've had at least one value near the high limit
+		if d := bp.Duration(i); !nearHigh && d > 29750*time.Millisecond {
+			nearHigh = true
+		}
+	}
+	if !(nearLow && nearHigh) {
+		t.Fatalf("Expected generated values near edges: near low [%t], near high [%t]", nearLow, nearHigh)
+	}
+}
+
+func TestJitterSmallLimits(t *testing.T) {
 	bp := SpecificBackoffPolicy{
 		Times: []time.Duration{
 			20 * time.Second,
@@ -184,5 +232,29 @@ func TestJitterSmall(t *testing.T) {
 		if d := bp.Duration(i); d < 15*time.Second || d > 25*time.Second {
 			t.Fatalf("On iteration %d, found unexpected value: %v\n", i, d)
 		}
+	}
+}
+
+func TestJitterSmallExtents(t *testing.T) {
+	bp := SpecificBackoffPolicy{
+		Times: []time.Duration{
+			20 * time.Second,
+		},
+		JitterMode: JitterSmall,
+	}
+
+	var nearLow, nearHigh bool
+	for i := 0; i < 1000; i++ {
+		// See if we've had at least one value near the low limit
+		if d := bp.Duration(i); !nearLow && d < 15250*time.Millisecond {
+			nearLow = true
+		}
+		// See if we've had at least one value near the high limit
+		if d := bp.Duration(i); !nearHigh && d > 24750*time.Millisecond {
+			nearHigh = true
+		}
+	}
+	if !(nearLow && nearHigh) {
+		t.Fatalf("Expected generated values near edges: near low [%t], near high [%t]", nearLow, nearHigh)
 	}
 }
