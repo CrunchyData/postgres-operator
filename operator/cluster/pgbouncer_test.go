d16927ab539ad5b1c2d5e05b8cfee55447e40aa0
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2020-05-06 23:14:59 -0400

    Move Postgres MD5 generation functions to internal
    While the MD5 password generation was consoliated, it effectively limited
    us to only supporting MD5. With the new interface, the PostgreSQL Operator
    is set up to support other methods (e.g. SCRAM).
    
    This also puts the code in the "internal" folder and adds some testing,
    which after a certain commit point will not be news.

---
+++ operator/cluster/pgbouncer_test.go
@@ -0,0 +1,40 @@
+package cluster
+
+/*
+ Copyright 2020 Crunchy Data Solutions, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+import (
+	"testing"
+
+	pgpassword "github.com/crunchydata/postgres-operator/internal/postgres/password"
+)
+
+func TestMakePostgresPassword(t *testing.T) {
+
+	t.Run("md5", func(t *testing.T) {
+		t.Run("valid", func(t *testing.T) {
+			passwordType := pgpassword.MD5
+			password := "datalake"
+			expected := "md56294153764d389dc6830b6ce4f923cdb"
+
+			actual := makePostgresPassword(passwordType, password)
+
+			if actual != expected {
+				t.Errorf("expected: %q actual: %q", expected, actual)
+			}
+		})
+
+	})
+}
