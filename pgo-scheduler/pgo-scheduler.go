70841aa3931c28b4143c77ae13f11af877def096
James Southard <james.southard@crunchydata.com>
2020-05-27 10:13:36 -0400

    moves logging library folder into internal
    This library is being moved into internal as step toward
    organizing the postgres-operator libraries. This reduces
    clutter in the top level project directory and supports
    future reorganization, refactoring, and movement to Go
    modules where appropriate.
    
    At this time, all internal libraries are believed to have
    postgres-operator specific code and are not likely to be
    dependencies of code outside of postgres-operator.
    
    However, should this be in err, each library move is split into a
    separate commit for easy reversion.

--- pgo-scheduler/pgo-scheduler.go
+++ pgo-scheduler/pgo-scheduler.go
@@ -28,7 +28,7 @@ import (
 	"github.com/crunchydata/postgres-operator/internal/controller"
 	nscontroller "github.com/crunchydata/postgres-operator/internal/controller/namespace"
 	"github.com/crunchydata/postgres-operator/internal/kubeapi"
-	crunchylog "github.com/crunchydata/postgres-operator/logging"
+	crunchylog "github.com/crunchydata/postgres-operator/internal/logging"
 	"github.com/crunchydata/postgres-operator/ns"
 	"github.com/crunchydata/postgres-operator/pgo-scheduler/scheduler"
 	sched "github.com/crunchydata/postgres-operator/pgo-scheduler/scheduler"
