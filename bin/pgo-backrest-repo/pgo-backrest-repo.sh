ee0e7548e13ea5e57a09fe8f95e8d4ea22fbffad
Andrew L'Ecuyer <andrew.lecuyer@crunchydata.com>
2020-05-28 20:59:51 +0000

    Hide Certain Errors in Backrest Repo Start Script
    With this change certain expected errors (i.e. the grep error
    indicating 'pgbackrest.conf' does not yet exist, or the mkdir error
    indicating  '~/.ssh/' dir already exists) are no longer displayed in
    the container logs for the backrest repo container.  This is to avoid
    confusion since these errors often result is confusion over whether
    or not an error condition truly has been occurred, and can
    and can often be red herrings when troubleshooting/researching various
    other error conditions.

--- bin/pgo-backrest-repo/pgo-backrest-repo.sh
+++ bin/pgo-backrest-repo/pgo-backrest-repo.sh
@@ -21,6 +21,8 @@ function trap_sigterm() {
 
 trap 'trap_sigterm' SIGINT SIGTERM
 
+echo "Starting the pgBackRest repo"
+
 CONFIG=/sshd
 REPO=/backrestrepo
 
@@ -38,23 +40,24 @@ fi
 # (i.e. the value specified by the container running the command via SSH is used instead), it is
 # simply set to a dummy directory within the config file.
 mkdir -p /tmp/pg1path
-if ! grep -Fxq "[${PGBACKREST_STANZA}]" "/etc/pgbackrest/pgbackrest.conf"
+if ! grep -Fxq "[${PGBACKREST_STANZA}]" "/etc/pgbackrest/pgbackrest.conf" 2> /dev/null
 then
-    printf "[%s]\npg1-path=/tmp/pg1path\n" "$PGBACKREST_STANZA" > /etc/pgbackrest/pgbackrest.conf
+    
+	printf "[%s]\npg1-path=/tmp/pg1path\n" "$PGBACKREST_STANZA" > /etc/pgbackrest/pgbackrest.conf
 
-		# Additionally, if the PGBACKREST S3 variables are set, add them here
-		if [[ "${PGBACKREST_REPO1_S3_KEY}" != "" ]]
-		then
-			printf "repo1-s3-key=%s\n" "${PGBACKREST_REPO1_S3_KEY}" >> /etc/pgbackrest/pgbackrest.conf
-		fi
+	# Additionally, if the PGBACKREST S3 variables are set, add them here
+	if [[ "${PGBACKREST_REPO1_S3_KEY}" != "" ]]
+	then
+		printf "repo1-s3-key=%s\n" "${PGBACKREST_REPO1_S3_KEY}" >> /etc/pgbackrest/pgbackrest.conf
+	fi
 
-		if [[ "${PGBACKREST_REPO1_S3_KEY_SECRET}" != "" ]]
-		then
-			printf "repo1-s3-key-secret=%s\n" "${PGBACKREST_REPO1_S3_KEY_SECRET}" >> /etc/pgbackrest/pgbackrest.conf
-		fi
+	if [[ "${PGBACKREST_REPO1_S3_KEY_SECRET}" != "" ]]
+	then
+		printf "repo1-s3-key-secret=%s\n" "${PGBACKREST_REPO1_S3_KEY_SECRET}" >> /etc/pgbackrest/pgbackrest.conf
+	fi
 fi
 
-mkdir ~/.ssh/
+mkdir -p ~/.ssh/
 cp $CONFIG/config ~/.ssh/
 #cp $CONFIG/authorized_keys ~/.ssh/
 cp $CONFIG/id_ed25519 /tmp
@@ -62,5 +65,7 @@ chmod 400 /tmp/id_ed25519 ~/.ssh/config
 
 # start sshd which is used by pgbackrest for remote connections
 /usr/sbin/sshd -D -f $CONFIG/sshd_config   &
+
+echo "The pgBackRest repo has been started"
 
 wait
