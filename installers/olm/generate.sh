6cec286858d1cd53067c7c3612fddd839ec66d96
Chris Bandy <chris.bandy@crunchydata.com>
2020-10-07 14:42:18 -0500

    Update OLM packaging to use python-yq
    The current version of `github.com/mikefarah/yq`, version 3.x, does not
    do the things we need: preserve formatting, strip comments, etc. We've
    been able to pin to an old version using the container image, but it
    still trips up new contributors.
    
    The current version of `https://github.com/kislyuk/yq` does what we need
    and even eliminates some of our jumping between YAML and JSON and `jq`.

--- installers/olm/generate.sh
+++ installers/olm/generate.sh
@@ -12,42 +12,38 @@ render < postgresql.package.yaml > "./package/${PACKAGE_NAME}.package.yaml"
 # ClusterServiceVersion filenames must end with '.clusterserviceversion.yaml'
 render < postgresoperator.csv.yaml > "./package/${PGO_VERSION}/postgresoperator.v${PGO_VERSION}.clusterserviceversion.yaml"
 
-crd_array="$( yq read --doc='*' --tojson postgresoperator.crd.yaml )"
-crd_names="$( jq <<< "$crd_array" --raw-output 'to_entries[] | [.key, .value.metadata.name] | @tsv' )"
+crd_names="$( yq --raw-output --slurp 'to_entries[] | [.key, .value.metadata.name] | @tsv' postgresoperator.crd.yaml )"
 
 # `operator-courier verify` expects only one CustomResourceDefinition per file.
 while IFS=$'\t' read index name; do
-	yq read --doc="$index" postgresoperator.crd.yaml > "./package/${PGO_VERSION}/${name}.crd.yaml"
+	yq --slurp --yaml-roundtrip ".[$index]" postgresoperator.crd.yaml > "./package/${PGO_VERSION}/${name}.crd.yaml"
 done <<< "$crd_names"
 
-yq_script="$( yq read --tojson "./package/${PGO_VERSION}/postgresoperator.v${PGO_VERSION}.clusterserviceversion.yaml" | jq \
-	--argjson images "$( yq read --tojson postgresoperator.csv.images.yaml | render )" \
-	--argjson crds "$( yq read --tojson postgresoperator.crd.descriptions.yaml | render )" \
-	--arg examples "$( yq read --tojson postgresoperator.crd.examples.yaml --doc='*' | render | jq . )" \
+yq --in-place --yaml-roundtrip \
+	--argjson images "$( yq '.' postgresoperator.csv.images.yaml | render )" \
+	--argjson crds "$( yq '.' postgresoperator.crd.descriptions.yaml | render )" \
+	--arg examples "$( yq --slurp '.' postgresoperator.crd.examples.yaml | render )" \
 	--arg description "$( render < description.upstream.md )" \
 	--arg icon "$( base64 ../seal.svg | tr -d '\n' )" \
-'{
-	"metadata.annotations.alm-examples": $examples,
-	"spec.customresourcedefinitions.owned": $crds,
-	"spec.description": $description,
-	"spec.icon": [{ mediatype: "image/svg+xml", base64data: $icon }],
+'
+	.metadata.annotations["alm-examples"] = $examples |
+	.spec.customresourcedefinitions.owned = $crds |
+	.spec.description = $description |
+	.spec.icon = [{ mediatype: "image/svg+xml", base64data: $icon }] |
 
-	"spec.install.spec.deployments[0].spec.template.spec.containers[0].env": (
-	.spec.install.spec.deployments[0].spec.template.spec.containers[0].env + $images),
-
-	"spec.install.spec.deployments[0].spec.template.spec.containers[1].env": (
-	.spec.install.spec.deployments[0].spec.template.spec.containers[1].env + $images)
-}' )"
-yq write --inplace --script=- <<< "$yq_script" "./package/${PGO_VERSION}/postgresoperator.v${PGO_VERSION}.clusterserviceversion.yaml"
+	.spec.install.spec.deployments[0].spec.template.spec.containers[0].env += $images |
+	.spec.install.spec.deployments[0].spec.template.spec.containers[1].env += $images |
+.' \
+	"./package/${PGO_VERSION}/postgresoperator.v${PGO_VERSION}.clusterserviceversion.yaml"
 
 if [ "${K8S_DISTRIBUTION:-}" = 'openshift' ]; then
-	yq_script="$( jq <<< '{}' \
+	yq --in-place --yaml-roundtrip \
 		--arg description "$( render < description.openshift.md )" \
-	'{
-		"spec.description": $description,
-		"spec.displayName": "Crunchy PostgreSQL for OpenShift",
-	}' )"
-	yq write --inplace --script=- <<< "$yq_script" "./package/${PGO_VERSION}/postgresoperator.v${PGO_VERSION}.clusterserviceversion.yaml"
+	'
+		.spec.description = $description |
+		.spec.displayName = "Crunchy PostgreSQL for OpenShift" |
+	.' \
+		"./package/${PGO_VERSION}/postgresoperator.v${PGO_VERSION}.clusterserviceversion.yaml"
 fi
 
 if > /dev/null command -v tree; then tree -C './package'; fi
