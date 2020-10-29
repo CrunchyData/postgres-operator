df9c390980bf0367ad7f4ce1e8677594de676816
tjmoore4 <42497036+tjmoore4@users.noreply.github.com>
2020-10-29 09:39:53 -0400

    Update pgMonitor version to v4.4-RC7
    Updates pgMonitor library references to v4.4-RC7.
    
    Issue: [ch9602]

--- bin/get-pgmonitor.sh
+++ bin/get-pgmonitor.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 echo "Getting pgMonitor..."
-PGMONITOR_COMMIT='v4.4-RC6'
+PGMONITOR_COMMIT='v4.4-RC7'
 
 # pgMonitor Setup
 if [[ -d ${PGOROOT?}/tools/pgmonitor ]]
