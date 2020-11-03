6d0dc8c2d5606e310631402b6bf0e846c8c07f0d
Chris Bandy <chris.bandy@crunchydata.com>
2020-11-03 12:03:30 -0600

    Initial structs and YAML for new API group
    Go and CRD are generated by "sigs.k8s.io/controller-tools".
    
    Issue: [ch9469]

---
+++ pkg/apis/postgres-operator.crunchydata.com/v1alpha1/postgrescluster_types.go
@@ -0,0 +1,67 @@
+/*
+Copyright 2020 Crunchy Data Solutions, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// PostgresClusterSpec defines the desired state of PostgresCluster
+type PostgresClusterSpec struct {
+
+	// TODO
+}
+
+// PostgresClusterStatus defines the observed state of PostgresCluster
+type PostgresClusterStatus struct {
+
+	// observedGeneration represents the .metadata.generation on which the status was based.
+	// +optional
+	// +kubebuilder:validation:Minimum=0
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// conditions represent the observations of postgrescluster's current state.
+	// Known .status.conditions.type are: TODO
+	// +optional
+	// +listType=map
+	// +listMapKey=type
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// PostgresCluster is the Schema for the postgresclusters API
+type PostgresCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PostgresClusterSpec   `json:"spec,omitempty"`
+	Status PostgresClusterStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// PostgresClusterList contains a list of PostgresCluster
+type PostgresClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PostgresCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PostgresCluster{}, &PostgresClusterList{})
+}
