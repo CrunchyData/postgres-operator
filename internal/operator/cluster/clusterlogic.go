fad89605ce7bf1355595f57c72f5e7011dc0d74d
Jonathan S. Katz <jkatz@users.noreply.github.com>
2020-11-27 11:54:40 -0500

    Fix crash in cluster shutdown logic
    The Operator would crash if a shutdown was issued but there was
    no in the cluster.
    
    Issue: [ch9825]
    Issue: #2073

--- internal/operator/cluster/clusterlogic.go
+++ internal/operator/cluster/clusterlogic.go
@@ -624,7 +624,10 @@ func ShutdownCluster(clientset kubernetes.Interface, restclient *rest.RESTClient
 		return err
 	}
 
-	if len(pods.Items) > 1 {
+	if len(pods.Items) == 0 {
+		return fmt.Errorf("Cluster Operator: Could not find primary pod for shutdown of "+
+			"cluster %s", cluster.Name)
+	} else if len(pods.Items) > 1 {
 		return fmt.Errorf("Cluster Operator: Invalid number of primary pods (%d) found when "+
 			"shutting down cluster %s", len(pods.Items), cluster.Name)
 	}
