0748231ced90e3ff1b09d988d3ea55df2e8e1da9
andrewlecuyer <43458182+andrewlecuyer@users.noreply.github.com>
2020-12-02 10:16:22 -0600

    Remove disabling of autofailover from rmdata
    The rmdata application no longer disables autofailover when deleting a
    PostgreSQL cluster.  In past versions of the PostgreSQL Operator, it
    was necessary to first disable autofailover prior to cluster deletion
    since the rmdata application itself would stop the PostgreSQL database
    using 'pg_ctl stop'.  However, since Patroni is now responsible for
    cleanly shutting down the database (specifically upon receipt of a
    SIGTERM signal), autofailver should no longer be disabled (if it is,
    Patroni will not respond to the SIGTERM and will therefore not attempt
    to cleanly shutdown the database).
    
    This commit therefore ensures an attempt is made to cleanly shutdown
    the database when deleting a PostgreSQL cluster.  This, in turn, will
    increase the likeliness that the cluster can later be recreated and
    cleanly restarted.
    
    Issue: [ch9856]

--- pgo-rmdata/rmdata/process.go
+++ pgo-rmdata/rmdata/process.go
@@ -52,15 +52,6 @@ const (
 func Delete(request Request) {
 	log.Infof("rmdata.Process %v", request)
 
-	// if, check to see if this is a full cluster removal...i.e. "IsReplica"
-	// and "IsBackup" is set to false
-	//
-	// if this is a full cluster removal, first disable autofailover
-	if !(request.IsReplica || request.IsBackup) {
-		log.Debug("disabling autofailover for cluster removal")
-		util.ToggleAutoFailover(request.Clientset, false, request.ClusterPGHAScope, request.Namespace)
-	}
-
 	//the case of 'pgo scaledown'
 	if request.IsReplica {
 		log.Info("rmdata.Process scaledown replica use case")
