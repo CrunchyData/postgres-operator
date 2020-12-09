52c3c19379bbf9a01a94243f7330f9a9fd7d1dbf
Jonathan S. Katz <jonathan.katz@excoventures.com>
2020-12-08 22:41:06 -0500

    Delete bootstrap Job once it successfully completes
    When the bootstrap Job completes successfully after a restore,
    it contains information that ends up being consumed by other
    parts of the Operator system, such as Patroni. As the logs
    from the Job do not provide much, if any, helpful information
    after a restore succeeds, it's best to have the Operator
    eliminate the job.
    
    As such, this changes the behavior so that the bootstrap Job
    is removed.
    
    As this has lead to some buggy behavior, this is being
    considered as a bug fix, as regular operational work would
    dictate that the Job is removed anyway.
    
    Issue: [ch9919]

--- internal/controller/job/bootstraphandler.go
+++ internal/controller/job/bootstraphandler.go
@@ -82,7 +82,7 @@ func (c *Controller) handleBootstrapUpdate(job *apiv1.Job) error {
 
 	// If the job was successful we updated the state of the pgcluster to a "bootstrapped" status.
 	// This will then trigger full initialization of the cluster.  We also cleanup any resources
-	// from the bootstrap job.
+	// from the bootstrap job and delete the job itself
 	if cluster.Status.State == crv1.PgclusterStateBootstrapping {
 
 		if err := c.cleanupBootstrapResources(job, cluster, restore); err != nil {
@@ -103,6 +103,11 @@ func (c *Controller) handleBootstrapUpdate(job *apiv1.Job) error {
 			log.Error(err)
 			return err
 		}
+
+		// as it is no longer needed, delete the job
+		deletePropagation := metav1.DeletePropagationBackground
+		return c.Client.BatchV1().Jobs(namespace).Delete(ctx, job.Name,
+			metav1.DeleteOptions{PropagationPolicy: &deletePropagation})
 	}
 
 	if restore {
