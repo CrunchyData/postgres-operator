57c9815ab3ad14575725aa15daab286407371dc7
Jonathan S. Katz <jkatz@users.noreply.github.com>
2020-12-10 12:38:36 -0500

    Only consider running Pods for `pgo test`
    By adding this limitation, Pods such as Evicted Pods would not
    be considered as a part of `pgo test` output, as this could
    present some odd scenarios, such as the presence of two primaries.
    
    Issue: [ch9931]
    Issue: #2095

--- internal/apiserver/clusterservice/clusterimpl.go
+++ internal/apiserver/clusterservice/clusterimpl.go
@@ -38,6 +38,7 @@ import (
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/client-go/kubernetes"
 )
@@ -2065,10 +2066,16 @@ func GetPrimaryAndReplicaPods(cluster *crv1.Pgcluster, ns string) ([]msgs.ShowCl
 	ctx := context.TODO()
 	output := make([]msgs.ShowClusterPod, 0)
 
+	// find all of the Pods that represent Postgres primary and replicas.
+	// only consider running Pods
 	selector := config.LABEL_SERVICE_NAME + "=" + cluster.Spec.Name + "," + config.LABEL_DEPLOYMENT_NAME
-	log.Debugf("selector for GetPrimaryAndReplicaPods is %s", selector)
 
-	pods, err := apiserver.Clientset.CoreV1().Pods(ns).List(ctx, metav1.ListOptions{LabelSelector: selector})
+	options := metav1.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector("status.phase", string(v1.PodRunning)).String(),
+		LabelSelector: selector,
+	}
+
+	pods, err := apiserver.Clientset.CoreV1().Pods(ns).List(ctx, options)
 	if err != nil {
 		return output, err
 	}
@@ -2088,9 +2095,12 @@ func GetPrimaryAndReplicaPods(cluster *crv1.Pgcluster, ns string) ([]msgs.ShowCl
 
 	}
 	selector = config.LABEL_SERVICE_NAME + "=" + cluster.Spec.Name + "-replica" + "," + config.LABEL_DEPLOYMENT_NAME
-	log.Debugf("selector for GetPrimaryAndReplicaPods is %s", selector)
+	options = metav1.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector("status.phase", string(v1.PodRunning)).String(),
+		LabelSelector: selector,
+	}
 
-	pods, err = apiserver.Clientset.CoreV1().Pods(ns).List(ctx, metav1.ListOptions{LabelSelector: selector})
+	pods, err = apiserver.Clientset.CoreV1().Pods(ns).List(ctx, options)
 	if err != nil {
 		return output, err
 	}
