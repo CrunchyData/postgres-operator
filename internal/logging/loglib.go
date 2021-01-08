24ac2cf86ab66e5a5c46fb1976437e58492279bf
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2021-01-08 14:40:24 -0500

    Happy New Year!
    Good riddance 2020, hello 2021.

--- internal/logging/loglib.go
+++ internal/logging/loglib.go
@@ -2,7 +2,7 @@
 package logging
 
 /*
- Copyright 2019 - 2020 Crunchy Data Solutions, Inc.
+ Copyright 2019 - 2021 Crunchy Data Solutions, Inc.
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
