3a0b03904d760717d56d694ea63ce3632658144c
TJ Moore <tj.moore@crunchydata.com>
2021-03-10 13:22:26 -0500

    Improve pki package tests
    This update improves the code coverage and
    simplifies the go tests for the root, intermediate
    and leaf certificate checks in the pki package.

--- internal/pki/intermediate_test.go
+++ internal/pki/intermediate_test.go
@@ -18,12 +18,15 @@ package pki
 import (
 	"context"
 	"crypto/ecdsa"
+	"crypto/rand"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"errors"
 	"fmt"
 	"math/big"
 	"reflect"
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
@@ -362,256 +365,134 @@ func TestParseIntermediateCertificateAuthority(t *testing.T) {
 }
 
 func TestIntermediateCAIsBad(t *testing.T) {
-	t.Run("Generate", func(t *testing.T) {
-		namespace := "pgo-test"
-		// run generate on rootCA to ensure it sets valid values
-		rootCA := NewRootCertificateAuthority()
-		if err := rootCA.Generate(); err != nil {
-			t.Fatalf("root certificate authority could not be generated")
+	ctx := context.Background()
+	testRoot, err := newTestRoot()
+	assert.NilError(t, err)
+
+	testIntermediate, err := newTestIntermediate(testRoot)
+	assert.NilError(t, err)
+
+	t.Run("intermediate cert is good", func(t *testing.T) {
+
+		assert.Assert(t, !IntermediateCAIsBad(ctx, testIntermediate, testRoot))
+	})
+
+	t.Run("intermediate cert is empty", func(t *testing.T) {
+
+		emptyIntermediate := &IntermediateCertificateAuthority{}
+		assert.Assert(t, IntermediateCAIsBad(ctx, emptyIntermediate, testRoot))
+	})
+
+	t.Run("error parsing root certificate", func(t *testing.T) {
+		testRoot.Certificate = &Certificate{
+			Certificate: []byte("notacert"),
 		}
 
-		t.Run("valid", func(t *testing.T) {
-			ca := &IntermediateCertificateAuthority{
-				Namespace:            namespace,
-				generateCertificate:  generateIntermediateCertificate,
-				generateKey:          generateKey,
-				generateSerialNumber: generateSerialNumber,
-			}
-
-			// run generate to ensure it sets valid values
-			if err := ca.Generate(rootCA); err != nil {
-				t.Fatalf("expected generate to return no errors, got: %s", err.Error())
-			}
-
-			// ensure private key and certificate are set
-			if ca.PrivateKey == nil {
-				t.Fatalf("expected private key to be set")
-			}
-
-			if ca.Certificate == nil {
-				t.Fatalf("expected certificate to be set")
-			}
-
-			if ca.PrivateKey.PrivateKey == nil {
-				t.Fatalf("expected private key to be set, got nil")
-			}
-
-			if len(ca.Certificate.Certificate) == 0 {
-				t.Fatalf("expected certificate to be generated")
-			}
-
-			// see if certificate can be parsed
-			x509Certificate, err := x509.ParseCertificate(ca.Certificate.Certificate)
-
-			if err != nil {
-				t.Fatalf("expected valid x509 ceriticate, actual %s", err.Error())
-			}
-
-			if !ca.PrivateKey.PrivateKey.PublicKey.Equal(x509Certificate.PublicKey) {
-				t.Fatalf("expected public key from stored key to match public key on certificate")
-			}
-
-			// check certain attributes
-			if !x509Certificate.IsCA {
-				t.Fatalf("expected certificate to be CA")
-			}
-
-			if !x509Certificate.MaxPathLenZero || x509Certificate.MaxPathLen != 0 {
-				t.Fatalf("expected max path length to be 0")
-			}
-
-			if x509Certificate.Issuer.CommonName != rootCAName {
-				t.Fatalf("expected issuer common name to be %s, actual %s",
-					rootCAName, x509Certificate.Issuer.CommonName)
-			}
-
-			commonName := fmt.Sprintf("%s.%s", namespace, rootCAName)
-			if x509Certificate.Subject.CommonName != commonName {
-				t.Fatalf("expected subject name to be %s, actual %s", commonName, x509Certificate.Subject.CommonName)
-			}
-
-			// ensure private key functions are set
-			assertConstructed(t, ca.PrivateKey)
-
-			// after completing all manual checks, ensure check functions returns false
-			ctx := context.Background()
-			assert.Assert(t, !IntermediateCAIsBad(ctx, ca, rootCA))
-		})
-
-		t.Run("invalid", func(t *testing.T) {
-			ctx := context.Background()
-
-			t.Run("generate certificate not set", func(t *testing.T) {
-				ca := &IntermediateCertificateAuthority{}
-				ca.generateCertificate = nil
-				ca.generateKey = generateKey
-				ca.generateSerialNumber = generateSerialNumber
-
-				if err := ca.Generate(rootCA); !errors.Is(err, ErrFunctionNotImplemented) {
-					t.Fatalf("expected function not implemented error")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("generate key not set", func(t *testing.T) {
-				ca := &IntermediateCertificateAuthority{}
-				ca.generateCertificate = generateIntermediateCertificate
-				ca.generateKey = nil
-				ca.generateSerialNumber = generateSerialNumber
-
-				if err := ca.Generate(rootCA); !errors.Is(err, ErrFunctionNotImplemented) {
-					t.Fatalf("expected function not implemented error")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("generate serial number not set", func(t *testing.T) {
-				ca := &IntermediateCertificateAuthority{}
-				ca.generateCertificate = generateIntermediateCertificate
-				ca.generateKey = generateKey
-				ca.generateSerialNumber = nil
-
-				if err := ca.Generate(rootCA); !errors.Is(err, ErrFunctionNotImplemented) {
-					t.Fatalf("expected function not implemented error")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("root certificate authority is nil", func(t *testing.T) {
-				ca := &IntermediateCertificateAuthority{}
-				ca.generateCertificate = generateIntermediateCertificate
-				ca.generateKey = generateKey
-				ca.generateSerialNumber = generateSerialNumber
-
-				if err := ca.Generate(nil); !errors.Is(err, ErrInvalidCertificateAuthority) {
-					t.Fatalf("expected invalid certificate authority")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("root certificate authority has no private key", func(t *testing.T) {
-				ca := &IntermediateCertificateAuthority{
-					Namespace:            namespace,
-					generateCertificate:  generateIntermediateCertificate,
-					generateKey:          generateKey,
-					generateSerialNumber: generateSerialNumber,
-				}
-				rootCA := NewRootCertificateAuthority()
-				if err := rootCA.Generate(); err != nil {
-					t.Fatalf("root certificate authority could not be generated")
-				}
-				rootCA.PrivateKey = nil
-
-				if err := ca.Generate(rootCA); !errors.Is(err, ErrInvalidCertificateAuthority) {
-					t.Fatalf("expected invalid certificate authority")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("root certificate authority has no certificate", func(t *testing.T) {
-				ca := &IntermediateCertificateAuthority{
-					Namespace:            namespace,
-					generateCertificate:  generateIntermediateCertificate,
-					generateKey:          generateKey,
-					generateSerialNumber: generateSerialNumber,
-				}
-				rootCA := NewRootCertificateAuthority()
-				if err := rootCA.Generate(); err != nil {
-					t.Fatalf("root certificate authority could not be generated")
-				}
-				rootCA.Certificate = nil
-
-				if err := ca.Generate(rootCA); !errors.Is(err, ErrInvalidCertificateAuthority) {
-					t.Fatalf("expected invalid certificate authority")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("root certificate authority has invalid certificate", func(t *testing.T) {
-				ca := &IntermediateCertificateAuthority{
-					Namespace:            namespace,
-					generateCertificate:  generateIntermediateCertificate,
-					generateKey:          generateKey,
-					generateSerialNumber: generateSerialNumber,
-				}
-				rootCA := NewRootCertificateAuthority()
-				if err := rootCA.Generate(); err != nil {
-					t.Fatalf("root certificate authority could not be generated")
-				}
-				rootCA.Certificate.Certificate = []byte{}
-
-				if err := ca.Generate(rootCA); err == nil {
-					t.Fatalf("expected certificate parsing error")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("cannot generate private key", func(t *testing.T) {
-				msg := "cannot generate private key"
-				ca := &IntermediateCertificateAuthority{
-					Namespace:            namespace,
-					generateCertificate:  generateIntermediateCertificate,
-					generateKey:          func() (*ecdsa.PrivateKey, error) { return nil, errors.New(msg) },
-					generateSerialNumber: generateSerialNumber,
-				}
-
-				if err := ca.Generate(rootCA); err.Error() != msg {
-					t.Fatalf("expected error: %s", msg)
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("cannot generate serial number", func(t *testing.T) {
-				msg := "cannot generate serial number"
-				ca := &IntermediateCertificateAuthority{
-					Namespace:            namespace,
-					generateCertificate:  generateIntermediateCertificate,
-					generateKey:          generateKey,
-					generateSerialNumber: func() (*big.Int, error) { return nil, errors.New(msg) },
-				}
-
-				if err := ca.Generate(rootCA); err.Error() != msg {
-					t.Fatalf("expected error: %s", msg)
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-
-			t.Run("cannot generate certificate", func(t *testing.T) {
-				msg := "cannot generate certificate"
-				ca := &IntermediateCertificateAuthority{
-					Namespace: namespace,
-					generateCertificate: func(*ecdsa.PrivateKey, *big.Int, *RootCertificateAuthority, string) ([]byte, error) {
-						return nil, errors.New(msg)
-					},
-					generateKey:          generateKey,
-					generateSerialNumber: generateSerialNumber,
-				}
-
-				if err := ca.Generate(rootCA); err.Error() != msg {
-					t.Fatalf("expected error: %s", msg)
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, IntermediateCAIsBad(ctx, ca, rootCA))
-			})
-		})
+		assert.Assert(t, IntermediateCAIsBad(ctx, testIntermediate, testRoot))
 	})
+
+	t.Run("error parsing intermediate certificate", func(t *testing.T) {
+
+		testRoot2, err := newTestRoot()
+		assert.NilError(t, err)
+
+		testIntermediate.Certificate = &Certificate{
+			Certificate: []byte("notacert"),
+		}
+
+		assert.Assert(t, IntermediateCAIsBad(ctx, testIntermediate, testRoot2))
+	})
+
+	t.Run("intermediate is not a CA", func(t *testing.T) {
+
+		testRoot3, err := newTestRoot()
+		assert.NilError(t, err)
+
+		badIntermediate := &IntermediateCertificateAuthority{}
+		badIntermediate.generateCertificate = generateIntermediateCertificateBadCA
+		badIntermediate.generateKey = generateKey
+		badIntermediate.generateSerialNumber = generateSerialNumber
+
+		// run generate to ensure it sets valid values
+		if err := badIntermediate.Generate(testRoot3); err != nil {
+			t.Fatalf("expected generate to return no errors, got: %s", err.Error())
+		}
+
+		assert.Assert(t, IntermediateCAIsBad(ctx, badIntermediate, testRoot3))
+
+	})
+
+	t.Run("intermediate verify fails", func(t *testing.T) {
+		testRoot4, err := newTestRoot()
+		assert.NilError(t, err)
+
+		testIntermediate5 := &IntermediateCertificateAuthority{}
+		testIntermediate5.generateCertificate = generateIntermediateCertificate
+		testIntermediate5.generateKey = generateKey
+		testIntermediate5.generateSerialNumber = generateSerialNumber
+
+		// run generate to ensure it sets valid values
+		if err := testRoot.Generate(); err != nil {
+			t.Fatalf("expected generate to return no errors, got: %s", err.Error())
+		}
+
+		if err := testIntermediate5.Generate(testRoot); err != nil {
+			t.Fatalf("expected generate to return no errors, got: %s", err.Error())
+		}
+
+		// wrong root
+		assert.Assert(t, IntermediateCAIsBad(context.Background(), testIntermediate5, testRoot4))
+
+	})
+}
+
+// generateIntermediateCertificateBadCA creates an intermediate certificate that is not
+// configured as a CA
+func generateIntermediateCertificateBadCA(privateKey *ecdsa.PrivateKey, serialNumber *big.Int,
+	rootCA *RootCertificateAuthority, namespace string) ([]byte, error) {
+	// first, ensure that the root certificate can be turned into a x509
+	// Certificate object so it can be used as the parent certificate when
+	// generating
+	if rootCA == nil || rootCA.Certificate == nil || rootCA.PrivateKey == nil {
+		return nil, fmt.Errorf("%w: root certificate authority needs to be generated",
+			ErrInvalidCertificateAuthority)
+	}
+
+	parent, err := x509.ParseCertificate(rootCA.Certificate.Certificate)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// prepare the certificate. set the validity time to the predefined range
+	now := time.Now()
+	template := &x509.Certificate{
+		BasicConstraintsValid: true,
+		IsCA:                  false,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		NotBefore:             now.Add(beforeInterval),
+		NotAfter:              now.Add(defaultIntermediateCAExpiration),
+		SerialNumber:          serialNumber,
+		SignatureAlgorithm:    certificateSignatureAlgorithm,
+		Subject: pkix.Name{
+			CommonName: fmt.Sprintf("%s.%s", namespace, rootCAName),
+		},
+	}
+
+	// create the intermediate certificate and sign it using the root CA
+	return x509.CreateCertificate(rand.Reader, template, parent,
+		privateKey.Public(), rootCA.PrivateKey.PrivateKey)
+}
+
+// newTestIntermediate creates a new test intermediate certificate
+func newTestIntermediate(testRoot *RootCertificateAuthority) (*IntermediateCertificateAuthority, error) {
+
+	testIntermediate := &IntermediateCertificateAuthority{}
+	testIntermediate.generateCertificate = generateIntermediateCertificate
+	testIntermediate.generateKey = generateKey
+	testIntermediate.generateSerialNumber = generateSerialNumber
+
+	err := testIntermediate.Generate(testRoot)
+
+	return testIntermediate, err
 }
--- internal/pki/root_test.go
+++ internal/pki/root_test.go
@@ -17,11 +17,14 @@ package pki
 
 import (
 	"crypto/ecdsa"
+	"crypto/rand"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"errors"
 	"math/big"
 	"reflect"
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
@@ -259,154 +262,119 @@ func TestRootCertificateAuthority(t *testing.T) {
 }
 
 func TestRootCAIsBad(t *testing.T) {
-	t.Run("Generate", func(t *testing.T) {
-		t.Run("valid", func(t *testing.T) {
-			ca := &RootCertificateAuthority{}
-			ca.generateCertificate = generateRootCertificate
-			ca.generateKey = generateKey
-			ca.generateSerialNumber = generateSerialNumber
+	rootCA, err := newTestRoot()
+	assert.NilError(t, err)
 
-			// run generate to ensure it sets valid values
-			if err := ca.Generate(); err != nil {
-				t.Fatalf("expected generate to return no errors, got: %s", err.Error())
-			}
+	t.Run("root cert is good", func(t *testing.T) {
 
-			// ensure private key and certificate are set
-			if ca.PrivateKey == nil {
-				t.Fatalf("expected private key to be set")
-			}
-
-			if ca.Certificate == nil {
-				t.Fatalf("expected certificate to be set")
-			}
-
-			if ca.PrivateKey.PrivateKey == nil {
-				t.Fatalf("expected private key to be set, got nil")
-			}
-
-			if len(ca.Certificate.Certificate) == 0 {
-				t.Fatalf("expected certificate to be generated")
-			}
-
-			// see if certificate can be parsed
-			x509Certificate, err := x509.ParseCertificate(ca.Certificate.Certificate)
-
-			if err != nil {
-				t.Fatalf("expected valid x509 ceriticate, actual %s", err.Error())
-			}
-
-			if !ca.PrivateKey.PrivateKey.PublicKey.Equal(x509Certificate.PublicKey) {
-				t.Fatalf("expected public keys to match")
-			}
-
-			// check certain attributes
-			if !x509Certificate.IsCA {
-				t.Fatalf("expected certificate to be CA")
-			}
-
-			if x509Certificate.MaxPathLen != 1 {
-				t.Fatalf("expected max path len to be 1, actual %d", x509Certificate.MaxPathLen)
-			}
-
-			if x509Certificate.Subject.CommonName != rootCAName {
-				t.Fatalf("expected subject name to be %s, actual %s", defaultRootCAExpiration, x509Certificate.Subject.CommonName)
-			}
-
-			// ensure private key functions are set
-			assertConstructed(t, ca.PrivateKey)
-
-			// after completing all manual checks, ensure check functions returns false
-			assert.Assert(t, !RootCAIsBad(ca))
-
-		})
-
-		t.Run("invalid", func(t *testing.T) {
-			t.Run("generate certificate not set", func(t *testing.T) {
-				ca := &RootCertificateAuthority{}
-				ca.generateCertificate = nil
-				ca.generateKey = generateKey
-				ca.generateSerialNumber = generateSerialNumber
-
-				if err := ca.Generate(); !errors.Is(err, ErrFunctionNotImplemented) {
-					t.Fatalf("expected function not implemented error")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, RootCAIsBad(ca))
-			})
-
-			t.Run("generate key not set", func(t *testing.T) {
-				ca := &RootCertificateAuthority{}
-				ca.generateCertificate = generateRootCertificate
-				ca.generateKey = nil
-				ca.generateSerialNumber = generateSerialNumber
-
-				if err := ca.Generate(); !errors.Is(err, ErrFunctionNotImplemented) {
-					t.Fatalf("expected function not implemented error")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, RootCAIsBad(ca))
-			})
-
-			t.Run("generate serial number not set", func(t *testing.T) {
-				ca := &RootCertificateAuthority{}
-				ca.generateCertificate = generateRootCertificate
-				ca.generateKey = generateKey
-				ca.generateSerialNumber = nil
-
-				if err := ca.Generate(); !errors.Is(err, ErrFunctionNotImplemented) {
-					t.Fatalf("expected function not implemented error")
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, RootCAIsBad(ca))
-			})
-
-			t.Run("cannot generate private key", func(t *testing.T) {
-				msg := "cannot generate private key"
-				ca := &RootCertificateAuthority{}
-				ca.generateCertificate = generateRootCertificate
-				ca.generateKey = func() (*ecdsa.PrivateKey, error) { return nil, errors.New(msg) }
-				ca.generateSerialNumber = generateSerialNumber
-
-				if err := ca.Generate(); err.Error() != msg {
-					t.Fatalf("expected error: %s", msg)
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, RootCAIsBad(ca))
-			})
-
-			t.Run("cannot generate serial number", func(t *testing.T) {
-				msg := "cannot generate serial number"
-				ca := &RootCertificateAuthority{}
-				ca.generateCertificate = generateRootCertificate
-				ca.generateKey = generateKey
-				ca.generateSerialNumber = func() (*big.Int, error) { return nil, errors.New(msg) }
-
-				if err := ca.Generate(); err.Error() != msg {
-					t.Fatalf("expected error: %s", msg)
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, RootCAIsBad(ca))
-			})
-
-			t.Run("cannot generate certificate", func(t *testing.T) {
-				msg := "cannot generate certificate"
-				ca := &RootCertificateAuthority{}
-				ca.generateCertificate = func(*ecdsa.PrivateKey, *big.Int) ([]byte, error) { return nil, errors.New(msg) }
-				ca.generateKey = generateKey
-				ca.generateSerialNumber = generateSerialNumber
-
-				if err := ca.Generate(); err.Error() != msg {
-					t.Fatalf("expected error: %s", msg)
-				}
-
-				// after completing manual check, ensure check functions returns true
-				assert.Assert(t, RootCAIsBad(ca))
-			})
-		})
+		assert.Assert(t, !RootCAIsBad(rootCA))
 	})
+
+	t.Run("intermediate cert is empty", func(t *testing.T) {
+
+		emptyRoot := &RootCertificateAuthority{}
+		assert.Assert(t, RootCAIsBad(emptyRoot))
+	})
+
+	t.Run("error parsing certificate", func(t *testing.T) {
+		rootCA.Certificate = &Certificate{
+			Certificate: []byte("notacert"),
+		}
+
+		assert.Assert(t, RootCAIsBad(rootCA))
+	})
+
+	t.Run("error is not a CA", func(t *testing.T) {
+
+		badCa := &RootCertificateAuthority{
+			generateCertificate:  generateRootCertificateBadCA,
+			generateKey:          generateKey,
+			generateSerialNumber: generateSerialNumber,
+		}
+
+		// run generate to ensure it sets valid values
+		if err := badCa.Generate(); err != nil {
+			t.Fatalf("expected generate to return no errors, got: %s", err.Error())
+		}
+
+		assert.Assert(t, RootCAIsBad(badCa))
+
+	})
+
+	t.Run("error expired", func(t *testing.T) {
+
+		badCa := &RootCertificateAuthority{
+			generateCertificate:  generateRootCertificateExpired,
+			generateKey:          generateKey,
+			generateSerialNumber: generateSerialNumber,
+		}
+
+		// run generate to ensure it sets valid values
+		if err := badCa.Generate(); err != nil {
+			t.Fatalf("expected generate to return no errors, got: %s", err.Error())
+		}
+
+		assert.Assert(t, RootCAIsBad(badCa))
+
+	})
+}
+
+// generateRootCertificateBadCA creates a root certificate that is not
+// configured as a CA
+func generateRootCertificateBadCA(privateKey *ecdsa.PrivateKey, serialNumber *big.Int) ([]byte, error) {
+	// prepare the certificate. set the validity time to the predefined range
+	now := time.Now()
+	template := &x509.Certificate{
+		BasicConstraintsValid: true,
+		IsCA:                  false,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		//MaxPathLen:            1, // as each namespace will have an intermediate CA
+		NotBefore:          now.Add(beforeInterval),
+		NotAfter:           now.Add(defaultRootCAExpiration),
+		SerialNumber:       serialNumber,
+		SignatureAlgorithm: certificateSignatureAlgorithm,
+		Subject: pkix.Name{
+			CommonName: rootCAName,
+		},
+	}
+
+	// a root certificate has no parent, so pass in the template twice
+	return x509.CreateCertificate(rand.Reader, template, template,
+		privateKey.Public(), privateKey)
+}
+
+// generateRootCertificateExpired creates a root certificate that is already expired
+func generateRootCertificateExpired(privateKey *ecdsa.PrivateKey, serialNumber *big.Int) ([]byte, error) {
+	// prepare the certificate. set the validity time to the predefined range
+	now := time.Now()
+	template := &x509.Certificate{
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		MaxPathLen:            1, // as each namespace will have an intermediate CA
+		NotBefore:             now.Add(beforeInterval),
+		NotAfter:              now.Add(beforeInterval), // not after an hour ago, i.e. expired
+		SerialNumber:          serialNumber,
+		SignatureAlgorithm:    certificateSignatureAlgorithm,
+		Subject: pkix.Name{
+			CommonName: rootCAName,
+		},
+	}
+
+	// a root certificate has no parent, so pass in the template twice
+	return x509.CreateCertificate(rand.Reader, template, template,
+		privateKey.Public(), privateKey)
+}
+
+// newTestRoot creates a new test root certificate
+func newTestRoot() (*RootCertificateAuthority, error) {
+	testRoot := &RootCertificateAuthority{}
+	testRoot.generateCertificate = generateRootCertificate
+	testRoot.generateKey = generateKey
+	testRoot.generateSerialNumber = generateSerialNumber
+
+	// run generate to ensure it sets valid values
+	err := testRoot.Generate()
+
+	return testRoot, err
 }
