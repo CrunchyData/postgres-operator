e1274416894163f4fab9cc9d28b99a55094978cc
Heath E. Lord <heath.lord@crunchydata.com>
2021-04-23 17:13:55 -0400

    Fix year

--- bin/license_aggregator.sh
+++ bin/license_aggregator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2016 - 2021 Crunchy Data Solutions, Inc.
+# Copyright 2021 Crunchy Data Solutions, Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
