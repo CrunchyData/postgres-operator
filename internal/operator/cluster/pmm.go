1dd6bfc7845d71570688a99f0e26ff3d201322f2
Max Dudin <dudin.mv@gmail.com>
2021-05-13 14:54:38 +0300

    K8SPG-25 Update imports

--- internal/operator/cluster/pmm.go
+++ internal/operator/cluster/pmm.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/percona/percona-postgresql-operator/internal/kubeapi"
-	"github.com/percona/percona-postgresql-operator/internal/operator"
-	crv1 "github.com/percona/percona-postgresql-operator/pkg/apis/crunchydata.com/v1"
+	"github.com/crunchydata/postgres-operator/internal/kubeapi"
+	"github.com/crunchydata/postgres-operator/internal/operator"
+	crv1 "github.com/crunchydata/postgres-operator/pkg/apis/crunchydata.com/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 )
