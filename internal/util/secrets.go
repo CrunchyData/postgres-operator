a0e6ea12c1f1379253c5eb6003f2d76359a6e965
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2021-06-04 17:02:47 -0400

    Delete a lot of unused PGO stuff
    This is no longer in the updated architecture.

--- internal/util/secrets.go
+++ internal/util/secrets.go
@@ -16,20 +16,12 @@ package util
 */
 
 import (
-	"context"
 	"crypto/rand"
-	"fmt"
 	"math/big"
 	"strconv"
 	"strings"
 
-	"github.com/crunchydata/postgres-operator/internal/config"
-	"github.com/crunchydata/postgres-operator/internal/kubeapi"
 	crv1 "github.com/crunchydata/postgres-operator/pkg/apis/crunchydata.com/v1"
-	log "github.com/sirupsen/logrus"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 )
 
 // UserSecretFormat follows the pattern of how the user information is stored,
@@ -40,6 +32,11 @@ const UserSecretFormat = "%s-%s" + crv1.UserSecretSuffix
 // information on these selections, please consulting the ASCII man page
 // (`man ascii`)
 const (
+	// DefaultGeneratedPasswordLength is the length of what a generated password
+	// is if it's not set in the pgo.yaml file, and to create some semblance of
+	// consistency
+	DefaultGeneratedPasswordLength = 24
+
 	// passwordCharLower is the lowest ASCII character to use for generating a
 	// password, which is 40
 	passwordCharLower = 40
@@ -56,24 +53,6 @@ const (
 // character for the password. Since the random integer generator looks for
 // values from [0,X), we need to force this to be [40,126]
 var passwordCharSelector = big.NewInt(passwordCharUpper - passwordCharLower)
-
-// CreateSecret create the secret, user, and primary secrets
-func CreateSecret(clientset kubernetes.Interface, db, secretName, username, password, namespace string) error {
-	ctx := context.TODO()
-	secret := v1.Secret{}
-
-	secret.Name = secretName
-	secret.ObjectMeta.Labels = make(map[string]string)
-	secret.ObjectMeta.Labels["pg-cluster"] = db
-	secret.ObjectMeta.Labels[config.LABEL_VENDOR] = config.LABEL_CRUNCHY
-	secret.Data = make(map[string][]byte)
-	secret.Data["username"] = []byte(username)
-	secret.Data["password"] = []byte(password)
-
-	_, err := clientset.CoreV1().Secrets(namespace).Create(ctx, &secret, metav1.CreateOptions{})
-
-	return err
-}
 
 // GeneratePassword generates a password of a given length out of the acceptable
 // ASCII characters suitable for a password
@@ -117,66 +96,4 @@ func GeneratedPasswordLength(configuredPasswordLength string) int {
 	}
 
 	return generatedPasswordLength
-}
-
-// GetPasswordFromSecret will fetch the password from a user secret
-func GetPasswordFromSecret(clientset kubernetes.Interface, namespace, secretName string) (string, error) {
-	ctx := context.TODO()
-	secret, err := clientset.CoreV1().Secrets(namespace).Get(ctx, secretName, metav1.GetOptions{})
-	if err != nil {
-		return "", err
-	}
-
-	return string(secret.Data["password"][:]), nil
-}
-
-// IsPostgreSQLUserSystemAccount determines whether or not this is a system
-// PostgreSQL user account, as if this returns true, one likely may not want to
-// allow a user to directly access the account
-// Normalizes the lookup by downcasing it
-func IsPostgreSQLUserSystemAccount(username string) bool {
-	// go look up and see if the username is in the map
-	_, found := crv1.PGUserSystemAccounts[strings.ToLower(username)]
-	return found
-}
-
-// CreateUserSecret will create a new secret holding a user credential
-func CreateUserSecret(clientset kubernetes.Interface, clustername, username, password, namespace string) error {
-	secretName := fmt.Sprintf(UserSecretFormat, clustername, username)
-
-	if err := CreateSecret(clientset, clustername, secretName, username, password, namespace); err != nil {
-		log.Error(err)
-		return err
-	}
-
-	return nil
-}
-
-// UpdateUserSecret updates a user secret with a new password. It follows the
-// following method:
-//
-// 1. If the Secret exists, it updates the value of the Secret
-// 2. If the Secret does not exist, it creates the secret
-func UpdateUserSecret(clientset kubernetes.Interface, clustername, username, password, namespace string) error {
-	ctx := context.TODO()
-	secretName := fmt.Sprintf(UserSecretFormat, clustername, username)
-
-	// see if the secret already exists
-	secret, err := clientset.CoreV1().Secrets(namespace).Get(ctx, secretName, metav1.GetOptions{})
-	// if this returns an error and it's not the "not found" error, return
-	// However, if it is the "not found" error, treat this as creating the user
-	// secret
-	if err != nil {
-		if !kubeapi.IsNotFound(err) {
-			return err
-		}
-
-		return CreateUserSecret(clientset, clustername, username, password, namespace)
-	}
-
-	// update the value of "password"
-	secret.Data["password"] = []byte(password)
-
-	_, err = clientset.CoreV1().Secrets(secret.Namespace).Update(ctx, secret, metav1.UpdateOptions{})
-	return err
 }
