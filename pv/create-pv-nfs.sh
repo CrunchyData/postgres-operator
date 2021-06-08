7763435aab6bee43da0945f07285f9c3da191d2f
galbenus <a.galbenus@inno-tek.it>
2021-06-08 11:12:56 +0300

    improve usability for pv/create-pv-nfs.sh script

--- pv/create-pv-nfs.sh
+++ pv/create-pv-nfs.sh
@@ -16,7 +16,7 @@
 # use NFS as the persistent volume storage area. It is **not** intended for
 # production.
 #
-# This script makes some assumptions, i.e:
+# By default this script makes some assumptions, i.e:
 #
 # - You have sudo
 # - You have your NFS filesystem mounted to the location you are running this
@@ -25,17 +25,90 @@
 # - Your PV names will be one of "crunchy-pvNNN" where NNN is a natural number
 # - Your NFS UID:GID is "nfsnobody:nfsnobody", which correspunds to "65534:65534"
 #
+# If you want to modify this script defaults, execute the script with -h argument to see the usage help
 # And awaaaay we go...
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PGO_NFS_IP=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
+PV_NAME_INPUT="crunchy-pv"
+OLD_PV_NAME_INPUT="crunchy-pv"
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+PV_COUNT=10
+NFS_MOUNT_PATH='/nfsfileshare'
+
+usage(){
+    echo "Usage: ./${SCRIPT_NAME} <options>"
+    echo "   
+  Options :
+    -n|--name                         persistent volume name. default is crunchy-pv
+    -v|--old-name                     old persisten volume name. Useful when you want to delete old PVs and use a new name.
+                                      default is crunchy-pv
+    -c|--count                        PV count. how many PVs to create. default is 10
+    -m|--nfs-mount                    nfs mount path. default is /nfsfileshare
+    -i|--nfs-ip                       nfs ip. default is ${PGO_NFS_IP}
+    -h|--help                         show usage
+    "
+    exit 1
+
+}
+
+if [[ $EUID > 0 ]] 
+  then echo "ERROR: Please run as root or use sudo"
+  usage
+fi
+
+opts=$(getopt \
+    -o n:o:c:hm:i: \
+    --long 'name:,old-name:,count:,help,nfs-mount:,nfs-ip: ' \
+    --name "${SCRIPT_NAME}" \
+    -- "$@"
+)
+
+eval set --$opts
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            usage
+            ;;
+        -m|--nfs-mount)
+            NFS_MOUNT_PATH=$2
+            shift 2
+            ;;
+        -n|--name)
+            PV_NAME_INPUT=$2
+            OLD_PV_NAME_INPUT=$2
+            shift 2
+            ;;
+        -o|--old-name)
+            OLD_PV_NAME_INPUT=$2
+            shift 2
+            ;;
+        -c|--count)
+            PV_COUNT=$2
+            shift 2
+            ;;
+        -i|--nfs-ip)
+            PGO_NFS_IP=$2
+            shift 2
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+
+PGO_CMD="kubectl"
 
 echo "create the test PV and PVC using the NFS dir"
-for i in {1..160}
+for i in $(seq 1 $PV_COUNT)
 do
-  PV_NAME="crunchy-pv${i}"
-  NFS_PV_PATH="/nfsfileshare/${PV_NAME}"
-
-  echo "deleting PV ${PV_NAME}"
-	$PGO_CMD delete pv "${PV_NAME}"
+  PV_NAME="${PV_NAME_INPUT}${i}"
+  OLD_PV_NAME="${OLD_PV_NAME_INPUT}${i}"
+  NFS_PV_PATH="${NFS_MOUNT_PATH}/${PV_NAME}"
+  
+  echo "deleting PV ${OLD_PV_NAME}"
+	$PGO_CMD delete pv "${OLD_PV_NAME}"
   sudo rm -rf "${NFS_PV_PATH}"
 
   # this is the manifest used to create the persistent volumes
@@ -67,5 +140,6 @@ EOF
   sudo chmod ugo=rwx "${NFS_PV_PATH}"
 
   # create the new persistent volume
+  echo "creating PV ${PV_NAME}"
   echo $MANIFEST | $PGO_CMD create -f -
 done
