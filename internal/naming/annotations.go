dce954bc526147bbf8e2b7f1ccf99532324d8d87
Jonathan S. Katz <jkatz@users.noreply.github.com>
2021-08-04 17:00:53 -0400

    Update internal/naming/annotations.go
    Co-authored-by: Chris Bandy <bandy.chris@gmail.com>

--- internal/naming/annotations.go
+++ internal/naming/annotations.go
@@ -18,7 +18,7 @@ package naming
 const (
 	annotationPrefix = labelPrefix
 
-	// Finalizer represents the annotaiton used to denote a finalizer.
+	// Finalizer marks an object to be garbage collected by this module.
 	Finalizer = annotationPrefix + "finalizer"
 
 	// PGBackRestBackup is the annotation that is added to a PostgresCluster to initiate a manual
