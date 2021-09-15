97958bb30b66977143abde773343f6a3647a32ef
Jonathan S. Katz <jonathan.katz@crunchydata.com>
2021-09-15 16:17:56 -0400

    Bump v4.7.3

--- pkg/apiservermsgs/common.go
+++ pkg/apiservermsgs/common.go
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const PGO_VERSION = "4.7.3-rc.1"
+const PGO_VERSION = "4.7.3"
 
 // Ok status
 const Ok = "ok"
