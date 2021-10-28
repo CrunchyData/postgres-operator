0475bb8d9b51725fb125fe163d04e917578590fe
ValClarkson <valerie.clarkson@crunchydata.com>
2021-10-28 19:26:07 +0000

    create pqsl table with kuttl test, added 2 scripts to run via kuttl to accomplish psql connection and table creation

---
+++ testing/kuttl/script/connectpsql.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PG_CLUSTER_USER_SECRET_NAME=test-pguser-test
+
+PGPASSWORD=$(kubectl get secrets -n postgres-operator "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.password | base64decode}}') \
+PGUSER=$(kubectl get secrets -n postgres-operator "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.user | base64decode}}') \
+PGDATABASE=$(kubectl get secrets -n postgres-operator "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.dbname | base64decode}}') \
+psql -h localhost -c "create table foo (x int);"
