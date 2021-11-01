9792946d3f0c2b24f2c5dce7fe7393af821c426e
ValClarkson <valerie.clarkson@crunchydata.com>
2021-11-01 21:29:44 +0000

    added env vars and renamed the cluster to cluster-start.  renamed files to follow conventions. updated command to select version() instead of create table, and updated kuttl to create namespaces

--- testing/kuttl/script/portforward.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PG_CLUSTER_PRIMARY_POD=$(kubectl get pod -n postgres-operator -o name -l postgres-operator.crunchydata.com/cluster=test,postgres-operator.crunchydata.com/role=master)
-kubectl -n postgres-operator port-forward "${PG_CLUSTER_PRIMARY_POD}" 5432:5432
