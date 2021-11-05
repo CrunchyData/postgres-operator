7c23dc75fc13dcd414d7096969572aa0e6f96aee
jmckulk <joseph.mckulka@crunchydata.com>
2021-11-05 15:36:41 -0400

    Update Kuttl tests to connect via a Kube Job
    Connecting to the cluster locally with automated tests can be finicky;
    This change uses a Kube job that can access internally (using the
    service to connect and mount the user secret as env variables).

--- testing/kuttl/script/connect-psql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CLUSTER=${CLUSTER:-default}
-PG_CLUSTER_USER_SECRET_NAME=${PG_CLUSTER_USER_SECRET_NAME:-$CLUSTER-pguser-$CLUSTER}
-
-PGPASSWORD=$(kubectl get secrets -n $NAMESPACE "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.password | base64decode}}') \
-PGUSER=$(kubectl get secrets -n $NAMESPACE "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.user | base64decode}}') \
-PGDATABASE=$(kubectl get secrets -n $NAMESPACE "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.dbname | base64decode}}') \
-psql -h localhost -c "select version();"
