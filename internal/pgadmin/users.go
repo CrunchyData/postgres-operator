f4a6fdf3811cd31eb4c85fa4a52e4e6f6412e2df
TJ Moore <tj.moore@crunchydata.com>
2021-11-30 16:12:39 -0500

    Manage pgAdmin server connections
    This commit creates server connections based on the users defined
    for the PostgresCluster. With this update, each user is able to
    log in with their unique password credential and manage their
    Postgres database sessions. Relevant changes, such as port or
    password, are handled automatically, but in some cases may require
    reloading the page.
    
    Issue: [sc-12538]

--- internal/pgadmin/users.go
+++ internal/pgadmin/users.go
@@ -19,9 +19,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 
 	"github.com/crunchydata/postgres-operator/internal/logging"
+	"github.com/crunchydata/postgres-operator/internal/naming"
 	"github.com/crunchydata/postgres-operator/pkg/apis/postgres-operator.crunchydata.com/v1beta1"
 )
 
@@ -34,7 +36,7 @@ type Executor func(
 // blocks that user from logging in to pgAdmin. The pgAdmin configuration
 // database must exist before calling this.
 func WriteUsersInPGAdmin(
-	ctx context.Context, exec Executor,
+	ctx context.Context, cluster *v1beta1.PostgresCluster, exec Executor,
 	users []v1beta1.PostgresUserSpec, passwords map[string]string,
 ) error {
 	// The location of pgAdmin files can vary by container image. Look for
@@ -72,11 +74,47 @@ if sys.path[0] != root:
 	//
 	// TODO(cbandy): pgAdmin v4.21 adds "auth_source" and "username" as required attributes.
 	// - https://git.postgresql.org/gitweb/?p=pgadmin4.git;f=web/pgadmin/model/__init__.py;hb=REL-4_21#l65
+
+	// When the users are created or modified, server groups and connections will
+	// also be configured, similar to the way server groups and connections are
+	// made with their respective dialog windows.
+	// - https://www.pgadmin.org/docs/pgadmin4/latest/server_group_dialog.html
+	// - https://www.pgadmin.org/docs/pgadmin4/latest/server_dialog.html
+
+	// We use a similar method to the import method when creating server connections
+	// - https://www.pgadmin.org/docs/pgadmin4/development/import_export_servers.html
+	// - https://git.postgresql.org/gitweb/?p=pgadmin4.git;f=web/setup.py;hb=REL-4_20#l256
+
+	// One server connection will be configured for each defined user.
+	// The name of the server connection will be the same as the cluster name.
+	// Note that the server connections are created when the users are created or
+	// modified. Changes to a server connection will generally persist until a
+	// change is made to the corresponding user. For custom server connections,
+	// a new server should be created with a unique name.
+
+	// The server connection password is the plaintext password encrypted with the
+	// password itself as the key.
+	// - https://git.postgresql.org/gitweb/?p=pgadmin4.git;;f=web/pgadmin/__init__.py;hb=REL-4_20#l580
+	// - https://git.postgresql.org/gitweb/?p=pgadmin4.git;f=web/pgadmin/utils/master_password.py;hb=REL-4_20#l20
+	// - https://git.postgresql.org/gitweb/?p=pgadmin4.git;;f=web/pgadmin/browser/server_groups/servers/__init__.py;hb=REL-4_20#l840
+
+	// Due to limitations on the types of updates that can be made to active server
+	// connections, when the current server connection is updated, we need to delete
+	// it and add a new server connection in its place. This will require a refresh
+	// if pgAdmin web GUI is being used when the update takes place.
+	// - https://git.postgresql.org/gitweb/?p=pgadmin4.git;f=web/pgadmin/browser/server_groups/servers/__init__.py;hb=REL-4_20#l604
+
+	// define the hostname of the primary service
+	primary := naming.ClusterPrimaryService(cluster)
+	hostname := primary.Name + "." + primary.Namespace + ".svc"
+
 	const script = `
+import copy
 import json
 import sys
 from pgadmin import create_app
-from pgadmin.model import db, Role, User
+from pgadmin.model import db, Role, User, Server, ServerGroup
+from pgadmin.utils.crypto import encrypt
 
 with create_app().app_context():
     admin = db.session.query(User).filter_by(id=1).first()
@@ -105,6 +143,54 @@ with create_app().app_context():
 
         db.session.add(user)
         db.session.commit()
+
+        # Set the cluster and host name variable.
+        (clustername, hostname, port) = sys.argv[1:]
+
+        # Get or create the group as necessary
+        group = (
+            db.session.query(ServerGroup).filter_by(
+                user_id=user.id,
+            ).order_by("id").first() or
+            ServerGroup()
+        )
+        group.name = "Crunchy PostgreSQL Operator"
+        group.user_id = user.id
+        db.session.add(group)
+        db.session.commit()
+
+        # Get or create the server connection.
+        server = (
+            db.session.query(Server).filter_by(
+                servergroup_id=group.id,
+                user_id=user.id,
+                name=clustername,
+            ).first() or
+            Server()
+        )
+
+        # Add the required values.
+        server.name = clustername
+        server.servergroup_id = group.id
+        server.user_id = user.id
+        server.ssl_mode = "prefer"
+        server.host = hostname
+        server.username = data['username']
+        # Save the encrypted server password.
+        server.password = encrypt(data['password'], data['password'])
+        server.maintenance_db = "postgres"
+        server.port = port
+
+        # If the existing server doesn't match our needed configuration, create
+        # a new one.
+        if server.id and db.session.is_modified(server):
+            old = copy.deepcopy(server)
+            db.make_transient(server)
+            server.id = None
+            db.session.delete(old)
+
+        db.session.add(server)
+        db.session.commit()
 `
 
 	var err error
@@ -125,7 +211,8 @@ with create_app().app_context():
 	}
 
 	if err == nil {
-		err = exec(ctx, &stdin, &stdout, &stderr, "python", "-c", search+script)
+		err = exec(ctx, &stdin, &stdout, &stderr, "python", "-c", search+script,
+			cluster.Name, hostname, fmt.Sprint(*cluster.Spec.Port))
 
 		log := logging.FromContext(ctx)
 		log.V(1).Info("wrote pgAdmin users",
