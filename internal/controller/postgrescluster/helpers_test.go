97022c929bf473e3eca282b3ac4b2924d5a02ae5
Chris Bandy <chris.bandy@crunchydata.com>
2021-12-01 15:10:46 -0600

    Bump component versions used in tests
    Issue: [sc-12859]

--- internal/controller/postgrescluster/helpers_test.go
+++ internal/controller/postgrescluster/helpers_test.go
@@ -42,9 +42,9 @@ import (
 var (
 	//TODO(tjmoore4): With the new RELATED_IMAGES defaulting behavior, tests could be refactored
 	// to reference those environment variables instead of hard coded image values
-	CrunchyPostgresHAImage = "registry.developers.crunchydata.com/crunchydata/crunchy-postgres-ha:centos8-13.3-4.7.0"
-	CrunchyPGBackRestImage = "registry.developers.crunchydata.com/crunchydata/crunchy-pgbackrest:centos8-13.3-4.7.0"
-	CrunchyPGBouncerImage  = "registry.developers.crunchydata.com/crunchydata/crunchy-pgbouncer:centos8-13.3-4.7.0"
+	CrunchyPostgresHAImage = "registry.developers.crunchydata.com/crunchydata/crunchy-postgres:centos8-13.5-0"
+	CrunchyPGBackRestImage = "registry.developers.crunchydata.com/crunchydata/crunchy-pgbackrest:centos8-2.36-0"
+	CrunchyPGBouncerImage  = "registry.developers.crunchydata.com/crunchydata/crunchy-pgbouncer:centos8-1.16-0"
 )
 
 // Scale extends d according to PGO_TEST_TIMEOUT_SCALE.
