679f0040cf572566b1515858e8efec4285d0dab9
jmckulk <joseph.mckulka@crunchydata.com>
2022-01-31 14:31:16 -0500

    Update json-patch package
    Move the json-patch package off of the incompatible module path.
    We have tests around this functionality and they pass before/after the
    change.

--- internal/controller/postgrescluster/apply.go
+++ internal/controller/postgrescluster/apply.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"reflect"
 
-	jsonpatch "github.com/evanphx/json-patch"
+	jsonpatch "github.com/evanphx/json-patch/v5"
 	"github.com/pkg/errors"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
