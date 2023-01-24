8d33281154976b7ef2c9c3c5c31383cc08e4a86c
Chris Bandy <chris.bandy@crunchydata.com>
2023-01-24 17:58:33 -0600

    New generic function to dereference a non-nil pointer

--- internal/initialize/primitives.go
+++ internal/initialize/primitives.go
@@ -25,11 +25,24 @@ func ByteMap(m *map[string][]byte) {
 	}
 }
 
+// FromPointer returns the value that p points to.
+// When p is nil, it returns the zero value of T.
+func FromPointer[T any](p *T) T {
+	var v T
+	if p != nil {
+		v = *p
+	}
+	return v
+}
+
 // Int32 returns a pointer to v.
 func Int32(v int32) *int32 { return &v }
 
 // Int64 returns a pointer to v.
 func Int64(v int64) *int64 { return &v }
+
+// Pointer returns a pointer to v.
+func Pointer[T any](v T) *T { return &v }
 
 // String returns a pointer to v.
 func String(v string) *string { return &v }
--- internal/initialize/primitives_test.go
+++ internal/initialize/primitives_test.go
@@ -56,6 +56,34 @@ func TestByteMap(t *testing.T) {
 	assert.DeepEqual(t, m, map[string][]byte{"x": []byte("y")})
 }
 
+func TestFromPointer(t *testing.T) {
+	t.Run("bool", func(t *testing.T) {
+		assert.Equal(t, initialize.FromPointer((*bool)(nil)), false)
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer(false)), false)
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer(true)), true)
+	})
+
+	t.Run("int32", func(t *testing.T) {
+		assert.Equal(t, initialize.FromPointer((*int32)(nil)), int32(0))
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer(int32(0))), int32(0))
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer(int32(-99))), int32(-99))
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer(int32(42))), int32(42))
+	})
+
+	t.Run("int64", func(t *testing.T) {
+		assert.Equal(t, initialize.FromPointer((*int64)(nil)), int64(0))
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer(int64(0))), int64(0))
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer(int64(-99))), int64(-99))
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer(int64(42))), int64(42))
+	})
+
+	t.Run("string", func(t *testing.T) {
+		assert.Equal(t, initialize.FromPointer((*string)(nil)), "")
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer("")), "")
+		assert.Equal(t, initialize.FromPointer(initialize.Pointer("sup")), "sup")
+	})
+}
+
 func TestInt32(t *testing.T) {
 	z := initialize.Int32(0)
 	if assert.Check(t, z != nil) {
@@ -88,6 +116,66 @@ func TestInt64(t *testing.T) {
 	if assert.Check(t, p != nil) {
 		assert.Equal(t, *p, int64(42))
 	}
+}
+
+func TestPointer(t *testing.T) {
+	t.Run("bool", func(t *testing.T) {
+		n := initialize.Pointer(false)
+		if assert.Check(t, n != nil) {
+			assert.Equal(t, *n, false)
+		}
+
+		y := initialize.Pointer(true)
+		if assert.Check(t, y != nil) {
+			assert.Equal(t, *y, true)
+		}
+	})
+
+	t.Run("int32", func(t *testing.T) {
+		z := initialize.Pointer(int32(0))
+		if assert.Check(t, z != nil) {
+			assert.Equal(t, *z, int32(0))
+		}
+
+		n := initialize.Pointer(int32(-99))
+		if assert.Check(t, n != nil) {
+			assert.Equal(t, *n, int32(-99))
+		}
+
+		p := initialize.Pointer(int32(42))
+		if assert.Check(t, p != nil) {
+			assert.Equal(t, *p, int32(42))
+		}
+	})
+
+	t.Run("int64", func(t *testing.T) {
+		z := initialize.Pointer(int64(0))
+		if assert.Check(t, z != nil) {
+			assert.Equal(t, *z, int64(0))
+		}
+
+		n := initialize.Pointer(int64(-99))
+		if assert.Check(t, n != nil) {
+			assert.Equal(t, *n, int64(-99))
+		}
+
+		p := initialize.Pointer(int64(42))
+		if assert.Check(t, p != nil) {
+			assert.Equal(t, *p, int64(42))
+		}
+	})
+
+	t.Run("string", func(t *testing.T) {
+		z := initialize.Pointer("")
+		if assert.Check(t, z != nil) {
+			assert.Equal(t, *z, "")
+		}
+
+		n := initialize.Pointer("sup")
+		if assert.Check(t, n != nil) {
+			assert.Equal(t, *n, "sup")
+		}
+	})
 }
 
 func TestString(t *testing.T) {
