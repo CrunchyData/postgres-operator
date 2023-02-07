4a4149c5deff264645a3a7fbd134d2c3dc8a53a4
Ben Blattberg <ben.blattberg@crunchydata.com>
2023-02-07 10:42:16 -0600

    Update Copyright notices for 2023

--- internal/operator/config/localdb.go
+++ internal/operator/config/localdb.go
@@ -1,7 +1,7 @@
 package config
 
 /*
- Copyright 2020 - 2022 Crunchy Data Solutions, Inl.
+ Copyright 2020 - 2023 Crunchy Data Solutions, Inl.
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
