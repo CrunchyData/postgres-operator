35d4974c918b7a3e11595fa0fb5d7caa00a7d386
ValClarkson <valerie0149@gmail.com>
2023-02-22 09:47:18 -0500

    release prep for v4.7.10
    Issue [sc-17754]

--- pkg/apiservermsgs/common.go
+++ pkg/apiservermsgs/common.go
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const PGO_VERSION = "4.7.9"
+const PGO_VERSION = "4.7.10"
 
 // Ok status
 const Ok = "ok"
