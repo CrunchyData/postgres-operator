4017a3ede9c9ee10edfcfce6244c854e0af5a1bb
Roman Gherta <roman.gherta@gmail.com>
2024-01-14 17:51:45 +0100

    pgadmin configuration keys can be alphanumeric see config.py docs

--- internal/pgadmin/config_test.go
+++ internal/pgadmin/config_test.go
@@ -55,7 +55,7 @@ func TestStartupCommand(t *testing.T) {
   import glob, json, re, os
   DEFAULT_BINARY_PATHS = {'pg': sorted([''] + glob.glob('/usr/pgsql-*/bin')).pop()}
   with open('/etc/pgadmin/conf.d/~postgres-operator/pgadmin.json') as _f:
-      _conf, _data = re.compile(r'[A-Z_]+'), json.load(_f)
+      _conf, _data = re.compile(r'[A-Z_0-9]+'), json.load(_f)
       if type(_data) is dict:
           globals().update({k: v for k, v in _data.items() if _conf.fullmatch(k)})
   if os.path.isfile('/etc/pgadmin/conf.d/~postgres-operator/ldap-bind-password'):
