7b70c599564f97362d4618e5096b852561b87ba4
Val <ValClarkson@users.noreply.github.com>
2024-02-06 17:28:27 -0500

    Update internal/bridge/crunchybridgecluster/crunchybridgecluster_controller.go
    Co-authored-by: Drew Sessler <36803518+dsessler7@users.noreply.github.com>

--- internal/bridge/crunchybridgecluster/crunchybridgecluster_controller.go
+++ internal/bridge/crunchybridgecluster/crunchybridgecluster_controller.go
@@ -43,7 +43,7 @@ import (
 
 const finalizer = "crunchybridgecluster.postgres-operator.crunchydata.com/finalizer"
 
-// crunchybridgeClusterReconciler reconciles a crunchybridgecluster object
+// CrunchyBridgeClusterReconciler reconciles a CrunchyBridgeCluster object
 type CrunchyBridgeClusterReconciler struct {
 	client.Client
 
