c3158a238b143225d29c2375e1731cfdfe408bbe
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-02-18 23:16:32 -0500

    spelling: nonexistent
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- internal/controller/postgrescluster/controller_ref_manager_test.go
+++ internal/controller/postgrescluster/controller_ref_manager_test.go
@@ -155,7 +155,7 @@ func TestManageControllerRefs(t *testing.T) {
 
 		obj := objBase.DeepCopy()
 		obj.Name = "ignore-no-postgrescluster"
-		obj.Labels = map[string]string{naming.LabelCluster: "noexist"}
+		obj.Labels = map[string]string{naming.LabelCluster: "nonexistent"}
 
 		if err := r.Client.Create(ctx, obj); err != nil {
 			t.Error(err)
