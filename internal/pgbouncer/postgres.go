8a1ad597c0d0ac01f2d68ab604aa41d75bd8dd4b
Chris Bandy <chris.bandy@crunchydata.com>
2024-11-03 20:34:36 -0600

    Move SQL escape functions to the Postgres package
    These are specific to Postgres. Add tests and remove unused functions.

--- internal/pgbouncer/postgres.go
+++ internal/pgbouncer/postgres.go
@@ -41,14 +41,14 @@ func sqlAuthenticationQuery(sqlFunctionName string) string {
 		// No replicators.
 		`NOT pg_authid.rolreplication`,
 		// Not the PgBouncer role itself.
-		`pg_authid.rolname <> ` + util.SQLQuoteLiteral(postgresqlUser),
+		`pg_authid.rolname <> ` + postgres.QuoteLiteral(postgresqlUser),
 		// Those without a password expiration or an expiration in the future.
 		`(pg_authid.rolvaliduntil IS NULL OR pg_authid.rolvaliduntil >= CURRENT_TIMESTAMP)`,
 	}, "\n    AND ")
 
 	return strings.TrimSpace(`
 CREATE OR REPLACE FUNCTION ` + sqlFunctionName + `(username TEXT)
-RETURNS TABLE(username TEXT, password TEXT) AS ` + util.SQLQuoteLiteral(`
+RETURNS TABLE(username TEXT, password TEXT) AS ` + postgres.QuoteLiteral(`
   SELECT rolname::TEXT, rolpassword::TEXT
   FROM pg_catalog.pg_authid
   WHERE pg_authid.rolname = $1
