d4483cc55060321da2cdfeacb3a9b75ebda1cc31
Chris Bandy <chris.bandy@crunchydata.com>
2025-02-10 09:40:47 -0600

    Add a function for setting permission on directories

---
+++ internal/shell/paths.go
@@ -0,0 +1,57 @@
+// Copyright 2024 - 2025 Crunchy Data Solutions, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// We want the [filepath] package to behave correctly for Linux containers.
+//go:build unix
+
+package shell
+
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+	"strings"
+)
+
+// MakeDirectories returns a list of POSIX shell commands that ensure each path
+// exists. It creates every directory leading to path from (but not including)
+// base and sets their permissions to exactly perms, regardless of umask.
+//
+// See:
+//   - https://pubs.opengroup.org/onlinepubs/9799919799/utilities/chmod.html
+//   - https://pubs.opengroup.org/onlinepubs/9799919799/utilities/mkdir.html
+//   - https://pubs.opengroup.org/onlinepubs/9799919799/utilities/test.html
+//   - https://pubs.opengroup.org/onlinepubs/9799919799/utilities/umask.html
+func MakeDirectories(perms fs.FileMode, base string, paths ...string) string {
+	// Without any paths, return a command that succeeds when the base path
+	// exists.
+	if len(paths) == 0 {
+		return `test -d ` + QuoteWord(base)
+	}
+
+	allPaths := append([]string(nil), paths...)
+	for _, p := range paths {
+		if r, err := filepath.Rel(base, p); err == nil && filepath.IsLocal(r) {
+			// The result of [filepath.Rel] is a shorter representation
+			// of the full path; skip it.
+			r = filepath.Dir(r)
+
+			for r != "." {
+				allPaths = append(allPaths, filepath.Join(base, r))
+				r = filepath.Dir(r)
+			}
+		}
+	}
+
+	return `` +
+		// Create all the paths and any missing parents.
+		`mkdir -p ` + strings.Join(QuoteWords(paths...), " ") +
+
+		// Set the permissions of every path and each parent.
+		// NOTE: FileMode bits other than file permissions are ignored.
+		fmt.Sprintf(` && chmod %#o %s`,
+			perms&fs.ModePerm,
+			strings.Join(QuoteWords(allPaths...), " "),
+		)
+}
