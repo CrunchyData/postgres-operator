69ecfbef0596ef7cde0e9eb12bfdc2bc9ce878b5
Chris Bandy <chris.bandy@crunchydata.com>
2025-02-28 14:39:04 -0600

    Send the "password" method to Postgres as "md5" instead
    The differences between "password," "md5," and "scram-sha-256" are not
    interesting to Postgres novices. This allows one to say "password" in
    the API and have secure authentication using usernames and passwords.
    
    The PGO default "password_encryption" has always been "scram-sha-256".
    
    Issue: PGO-2263

--- internal/controller/postgrescluster/postgres_test.go
+++ internal/controller/postgrescluster/postgres_test.go
@@ -59,6 +59,11 @@ func TestGeneratePostgresHBA(t *testing.T) {
 			rule:     `{ connection: hostssl, method: md5, options: { clientcert: verify-ca } }`,
 			expected: `"hostssl" all all all "md5"  "clientcert"="verify-ca"`,
 		},
+		// "password" input should be "md5" output
+		{
+			rule:     `{ connection: hostssl, method: password }`,
+			expected: `"hostssl" all all all "md5"`,
+		},
 	} {
 		var rule *v1beta1.PostgresHBARule
 		require.UnmarshalInto(t, &rule, tt.rule)
