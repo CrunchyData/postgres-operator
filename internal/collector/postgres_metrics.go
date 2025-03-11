447360bed8fd650e99eab39b7050986e5bb6d44f
Ben Blattberg <ben.blattberg@crunchydata.com>
2025-03-11 16:22:30 -0500

    missed ref to pgmonitor when resolving

--- internal/collector/postgres_metrics.go
+++ internal/collector/postgres_metrics.go
@@ -12,7 +12,6 @@ import (
 	"slices"
 	"strconv"
 
-	"github.com/crunchydata/postgres-operator/internal/feature"
 	"github.com/crunchydata/postgres-operator/internal/logging"
 	"github.com/crunchydata/postgres-operator/pkg/apis/postgres-operator.crunchydata.com/v1beta1"
 )
@@ -171,7 +170,7 @@ func EnablePostgresMetrics(ctx context.Context, inCluster *v1beta1.PostgresClust
 					"driver": "postgres",
 					"datasource": fmt.Sprintf(
 						`host=localhost dbname=postgres port=5432 user=%s password=${env:PGPASSWORD}`,
-						pgmonitor.MonitoringUser),
+						MonitoringUser),
 					"collection_interval": querySet.CollectionInterval,
 					// Give Postgres time to finish setup.
 					"initial_delay": "10s",
