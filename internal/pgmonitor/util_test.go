46ee186790703381717f46d2c1915f14225edd7a
Benjamin Blattberg <ben.blattberg@crunchydata.com>
2025-03-19 11:25:59 -0500

    Prefer OTEL to exporter if both enabled (#4137)
    If both Exporter and OTel Metrics are enabled,
    CPK prefers OTel.

--- internal/pgmonitor/util_test.go
+++ internal/pgmonitor/util_test.go
@@ -10,6 +10,8 @@ import (
 
 	"gotest.tools/v3/assert"
 
+	"github.com/crunchydata/postgres-operator/internal/feature"
+	"github.com/crunchydata/postgres-operator/internal/testing/require"
 	"github.com/crunchydata/postgres-operator/pkg/apis/postgres-operator.crunchydata.com/v1beta1"
 )
 
@@ -26,4 +28,19 @@ func TestExporterEnabled(t *testing.T) {
 
 	cluster.Spec.Monitoring.PGMonitor.Exporter = &v1beta1.ExporterSpec{}
 	assert.Assert(t, ExporterEnabled(ctx, cluster))
+
+	// Enabling the OpenTelemetryMetrics is not sufficient to disable the exporter
+	gate := feature.NewGate()
+	assert.NilError(t, gate.SetFromMap(map[string]bool{
+		feature.OpenTelemetryMetrics: true,
+	}))
+	ctx = feature.NewContext(ctx, gate)
+	assert.Assert(t, ExporterEnabled(ctx, cluster))
+
+	require.UnmarshalInto(t, &cluster.Spec, `{
+		instrumentation: {
+			logs: { retentionPeriod: 5h },
+		},
+	}`)
+	assert.Assert(t, !ExporterEnabled(ctx, cluster))
 }
