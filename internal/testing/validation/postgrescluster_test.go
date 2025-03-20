c0cf72a25ffb5e4cb03ceea05bd476c342f0f728
Chris Bandy <chris.bandy@crunchydata.com>
2025-03-20 10:37:36 -0500

    Ensure required LDAP HBA options are present
    Issue: PGO-2263

--- internal/testing/validation/postgrescluster_test.go
+++ internal/testing/validation/postgrescluster_test.go
@@ -118,6 +118,121 @@ func TestPostgresAuthenticationRules(t *testing.T) {
 			assert.Assert(t, cmp.Contains(cause.Message, "unsafe"))
 		}
 	})
+
+	t.Run("LDAP", func(t *testing.T) {
+		t.Run("Required", func(t *testing.T) {
+			cluster := base.DeepCopy()
+			require.UnmarshalInto(t, &cluster.Spec.Authentication, `{
+				rules: [
+					{ connection: hostssl, method: ldap },
+					{ connection: hostssl, method: ldap, options: {} },
+					{ connection: hostssl, method: ldap, options: { ldapbinddn: any } },
+				],
+			}`)
+
+			err := cc.Create(ctx, cluster, client.DryRunAll)
+			assert.Assert(t, apierrors.IsInvalid(err))
+
+			status := require.StatusError(t, err)
+			assert.Assert(t, status.Details != nil)
+			assert.Assert(t, cmp.Len(status.Details.Causes, 3))
+
+			for i, cause := range status.Details.Causes {
+				assert.Equal(t, cause.Field, fmt.Sprintf("spec.authentication.rules[%d]", i), "%#v", cause)
+				assert.Assert(t, cmp.Contains(cause.Message, `"ldap" method requires`))
+			}
+
+			// These are valid.
+
+			cluster.Spec.Authentication = nil
+			require.UnmarshalInto(t, &cluster.Spec.Authentication, `{
+				rules: [
+					{ connection: hostssl, method: ldap, options: { ldapbasedn: any } },
+					{ connection: hostssl, method: ldap, options: { ldapprefix: any } },
+					{ connection: hostssl, method: ldap, options: { ldapsuffix: any } },
+				],
+			}`)
+			assert.NilError(t, cc.Create(ctx, cluster, client.DryRunAll))
+		})
+
+		t.Run("Mixed", func(t *testing.T) {
+			// Some options cannot be combined with others.
+
+			cluster := base.DeepCopy()
+			require.UnmarshalInto(t, &cluster.Spec.Authentication, `{
+				rules: [
+					{ connection: hostssl, method: ldap, options: { ldapbinddn: any, ldapprefix: other } },
+					{ connection: hostssl, method: ldap, options: { ldapbasedn: any, ldapsuffix: other } },
+				],
+			}`)
+
+			err := cc.Create(ctx, cluster, client.DryRunAll)
+			assert.Assert(t, apierrors.IsInvalid(err))
+
+			status := require.StatusError(t, err)
+			assert.Assert(t, status.Details != nil)
+			assert.Assert(t, cmp.Len(status.Details.Causes, 2))
+
+			for i, cause := range status.Details.Causes {
+				assert.Equal(t, cause.Field, fmt.Sprintf("spec.authentication.rules[%d]", i), "%#v", cause)
+				assert.Assert(t, cmp.Regexp(`cannot use .+? options with .+? options`, cause.Message))
+			}
+
+			// These combinations are allowed.
+
+			cluster.Spec.Authentication = nil
+			require.UnmarshalInto(t, &cluster.Spec.Authentication, `{
+				rules: [
+					{ connection: hostssl, method: ldap, options: { ldapprefix: one, ldapsuffix: two } },
+					{ connection: hostssl, method: ldap, options: { ldapbasedn: one, ldapbinddn: two } },
+					{ connection: hostssl, method: ldap, options: {
+						ldapbasedn: one, ldapsearchattribute: two, ldapsearchfilter: three,
+					} },
+				],
+			}`)
+			assert.NilError(t, cc.Create(ctx, cluster, client.DryRunAll))
+		})
+	})
+
+	t.Run("RADIUS", func(t *testing.T) {
+		t.Run("Required", func(t *testing.T) {
+			cluster := base.DeepCopy()
+			require.UnmarshalInto(t, &cluster.Spec.Authentication, `{
+				rules: [
+					{ connection: hostssl, method: radius },
+					{ connection: hostssl, method: radius, options: {} },
+					{ connection: hostssl, method: radius, options: { radiusidentifiers: any } },
+					{ connection: hostssl, method: radius, options: { radiusservers: any } },
+					{ connection: hostssl, method: radius, options: { radiussecrets: any } },
+				],
+			}`)
+
+			err := cc.Create(ctx, cluster, client.DryRunAll)
+			assert.Assert(t, apierrors.IsInvalid(err))
+
+			status := require.StatusError(t, err)
+			assert.Assert(t, status.Details != nil)
+			assert.Assert(t, cmp.Len(status.Details.Causes, 5))
+
+			for i, cause := range status.Details.Causes {
+				assert.Equal(t, cause.Field, fmt.Sprintf("spec.authentication.rules[%d]", i), "%#v", cause)
+				assert.Assert(t, cmp.Contains(cause.Message, `"radius" method requires`))
+			}
+
+			// These are valid.
+
+			cluster.Spec.Authentication = nil
+			require.UnmarshalInto(t, &cluster.Spec.Authentication, `{
+				rules: [
+					{ connection: hostssl, method: radius, options: { radiusservers: one, radiussecrets: two } },
+					{ connection: hostssl, method: radius, options: {
+						radiusservers: one, radiussecrets: two, radiusports: three,
+					} },
+				],
+			}`)
+			assert.NilError(t, cc.Create(ctx, cluster, client.DryRunAll))
+		})
+	})
 }
 
 func TestPostgresConfigParameters(t *testing.T) {
