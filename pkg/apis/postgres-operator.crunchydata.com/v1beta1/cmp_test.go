d1e34b0d5d97ec106bf895e13bf215cf7892723e
Chris Bandy <chris.bandy@crunchydata.com>
2025-09-06 21:55:56 -0500

    Allow indented YAML in tests
    This allows Go tests to indent multi-line YAML literals to match the
    surrounding code/tests. Go code is indented with tabs, so tabs are
    converted to match the encoder's two-space indentation.

--- pkg/apis/postgres-operator.crunchydata.com/v1beta1/cmp_test.go
+++ pkg/apis/postgres-operator.crunchydata.com/v1beta1/cmp_test.go
@@ -5,8 +5,12 @@
 package v1beta1_test
 
 import (
-	"bytes"
+	"fmt"
+	"regexp"
+	"strings"
+	"testing"
 
+	"gotest.tools/v3/assert"
 	"gotest.tools/v3/assert/cmp"
 	"sigs.k8s.io/yaml"
 )
@@ -17,7 +21,116 @@ func MarshalsTo[T []byte | string](x any, y T) cmp.Comparison {
 	if err != nil {
 		return func() cmp.Result { return cmp.ResultFromError(err) }
 	}
-	return cmp.DeepEqual(string(b), string(
-		append(bytes.TrimLeft(bytes.TrimRight([]byte(y), "\t\n"), "\n"), '\n'),
+	return cmp.DeepEqual(string(b), dedentLines(
+		strings.TrimLeft(strings.TrimRight(string(y), "\t\n"), "\n"), 2,
 	))
+}
+
+var leadingTabs = regexp.MustCompile(`^\t+`)
+
+// dedentLines finds the shortest leading whitespace of every line in data and then removes it from every line.
+// When tabWidth is positive, leading tabs are converted to spaces first.
+func dedentLines(data string, tabWidth int) string {
+	if len(data) < 1 {
+		return ""
+	}
+
+	var lines = make([]string, 0, 20)
+	var lowest, highest string
+
+	for line := range strings.Lines(data) {
+		tabs := leadingTabs.FindString(line)
+
+		// Replace any leading tabs with spaces when tabWidth is positive.
+		// NOTE: [strings.Repeat] has a fast-path for spaces.
+		if need := tabWidth * len(tabs); need > 0 {
+			line = strings.Repeat(" ", need) + line[len(tabs):]
+		}
+
+		switch {
+		case lowest == "", highest == "":
+			lowest, highest = line, line
+
+		case len(strings.TrimSpace(line)) > 0:
+			lowest = min(lowest, line)
+			highest = max(highest, line)
+		}
+
+		lines = append(lines, line)
+	}
+
+	// This treats one tab the same as one space.
+	// That is, it expects all lines to be indented using spaces or using tabs; not both.
+	if width := func() int {
+		for i := range lowest {
+			if (lowest[i] != ' ' && lowest[i] != '\t') || lowest[i] != highest[i] {
+				return i
+			}
+		}
+		return len(lowest)
+	}(); width > 0 {
+		for i := range lines {
+			if len(lines[i]) > width {
+				lines[i] = lines[i][width:]
+			} else {
+				lines[i] = "\n"
+			}
+		}
+	}
+
+	return strings.TrimSuffix(strings.Join(lines, ""), "\n") + "\n"
+}
+
+func TestDedentLines(t *testing.T) {
+	for _, tc := range []struct {
+		width    int
+		input    string
+		expected string
+	}{
+		// empty stays that way
+		{width: 0, input: "", expected: ""},
+		{width: 1, input: "", expected: ""},
+		{width: 2, input: "", expected: ""},
+
+		// adds a missing newline
+		{input: "\n", expected: "\n"},
+		{input: "x", expected: "x\n"},
+		{input: "x\n", expected: "x\n"},
+		{input: "x\n\n", expected: "x\n\n"},
+
+		// width does not affect whats removed
+		{width: 2, input: "x", expected: "x\n"},
+		{width: 2, input: "\tx", expected: "x\n"},
+		{width: 2, input: " x", expected: "x\n"},
+		{width: 2, input: "  x", expected: "x\n"},
+		{width: 2, input: "   x", expected: "x\n"},
+
+		// positive width changes tabs to spaces
+		{width: 0, input: "\t\t~\n\t~\n", expected: "\t~\n~\n"},
+		{width: 1, input: "\t\t~\n\t~\n", expected: " ~\n~\n"},
+		{width: 2, input: "\t\t~\n\t~\n", expected: "  ~\n~\n"},
+
+		// width does not affect spaces
+		{width: 0, input: "  ~\n ~\n", expected: " ~\n~\n"},
+		{width: 1, input: "  ~\n ~\n", expected: " ~\n~\n"},
+		{width: 2, input: "  ~\n ~\n", expected: " ~\n~\n"},
+
+		// smallest indent can be anywhere
+		{input: " ~\n  ~\n  ~\n", expected: "~\n ~\n ~\n"},
+		{input: "  ~\n ~\n  ~\n", expected: " ~\n~\n ~\n"},
+		{input: "  ~\n  ~\n ~\n", expected: " ~\n ~\n~\n"},
+
+		// entirely whitespace becomes newline
+		{input: " ", expected: "\n"},
+		{input: "  ", expected: "\n"},
+		{input: "\t", expected: "\n"},
+		{input: "\t\t", expected: "\n"},
+
+		// blank lines preserved
+		{input: " ~\n\n ~\n", expected: "~\n\n~\n"},
+	} {
+		t.Run(fmt.Sprintf("%v:%#v", tc.width, tc.input), func(t *testing.T) {
+			assert.DeepEqual(t, dedentLines(tc.input, tc.width), tc.expected)
+		})
+	}
 }
