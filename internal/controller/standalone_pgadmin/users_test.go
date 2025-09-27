c7c07f49274d4e0ab6778b588bdcf4ef665c2232
TJ Moore <tj.moore@crunchydata.com>
2025-09-27 19:30:08 -0400

    Remove reconcilePGAdminVersion, adjust tests and add clarifying comments

--- internal/controller/standalone_pgadmin/users_test.go
+++ internal/controller/standalone_pgadmin/users_test.go
@@ -195,52 +195,40 @@ func TestReconcilePGAdminUsers(t *testing.T) {
 		assert.Equal(t, pgadmin.Status.MinorVersion, "")
 		assert.Equal(t, pgadmin.Status.ImageSHA, "")
 	})
-}
-
-func TestReconcilePGAdminVersion(t *testing.T) {
-	ctx := context.Background()
-	pod := corev1.Pod{}
-	pod.Namespace = "test-namespace"
-	pod.Name = "pgadmin-123-0"
-	reconciler := &PGAdminReconciler{}
-
-	podExecutor := func(
-		ctx context.Context, stdin io.Reader, stdout, stderr io.Writer, command ...string,
-	) error {
-		return reconciler.PodExec(ctx, pod.Namespace, pod.Name, "pgadmin", stdin, stdout, stderr, command...)
-	}
-
-	t.Run("SuccessfulRetrieval", func(t *testing.T) {
-		reconciler.PodExec = func(
-			ctx context.Context, namespace, pod, container string,
-			stdin io.Reader, stdout, stderr io.Writer, command ...string,
-		) error {
-			assert.Equal(t, pod, "pgadmin-123-0")
-			assert.Equal(t, namespace, "test-namespace")
-			assert.Equal(t, container, naming.ContainerPGAdmin)
-
-			// Simulate a v9.3 version of pgAdmin by setting stdout to "9.3"
-			// for podexec call in reconcilePGAdminVersion
-			_, _ = stdout.Write([]byte("9.3"))
-			return nil
-		}
-
-		version, err := reconciler.reconcilePGAdminVersion(ctx, podExecutor)
-		assert.NilError(t, err)
-		assert.Equal(t, version, "9.3")
-	})
 
 	t.Run("PodExecError", func(t *testing.T) {
-		reconciler.PodExec = func(
+		pgadmin := pgadmin.DeepCopy()
+		pod := pod.DeepCopy()
+
+		pod.DeletionTimestamp = nil
+		pod.Status.ContainerStatuses =
+			[]corev1.ContainerStatus{{Name: naming.ContainerPGAdmin}}
+		pod.Status.ContainerStatuses[0].State.Running =
+			new(corev1.ContainerStateRunning)
+		pod.Status.ContainerStatuses[0].ImageID = "fakeSHA"
+
+		r := new(PGAdminReconciler)
+		r.Client = fake.NewClientBuilder().WithObjects(pod).Build()
+
+		calls := 0
+		r.PodExec = func(
 			ctx context.Context, namespace, pod, container string,
 			stdin io.Reader, stdout, stderr io.Writer, command ...string,
 		) error {
+			calls++
+
+			assert.Equal(t, pod, "pgadmin-123-0")
+			assert.Equal(t, namespace, pgadmin.Namespace)
+			assert.Equal(t, container, naming.ContainerPGAdmin)
+
 			return errors.New("PodExecError")
 		}
 
-		version, err := reconciler.reconcilePGAdminVersion(ctx, podExecutor)
-		assert.Check(t, err != nil)
-		assert.Equal(t, version, "")
+		assert.Error(t, r.reconcilePGAdminUsers(ctx, pgadmin), "PodExecError")
+		assert.Equal(t, calls, 1, "PodExec should be called once")
+		assert.Equal(t, pgadmin.Status.MajorVersion, 0)
+		assert.Equal(t, pgadmin.Status.MinorVersion, "")
+		assert.Equal(t, pgadmin.Status.ImageSHA, "")
 	})
 }
 
