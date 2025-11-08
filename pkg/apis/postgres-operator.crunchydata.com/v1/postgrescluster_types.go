9667cfccaef509309ec4e9edf4ef3976c69d5eeb
Drew Sessler <drew.sessler@crunchydata.com>
2025-11-08 02:31:36 -0800

    Add rule to prevent users from attempting to use the ssl_groups parameter with pg17 or earlier.

--- pkg/apis/postgres-operator.crunchydata.com/v1/postgrescluster_types.go
+++ pkg/apis/postgres-operator.crunchydata.com/v1/postgrescluster_types.go
@@ -17,6 +17,10 @@ import (
 // PostgresClusterSpec defines the desired state of PostgresCluster
 // ---
 //
+// # Postgres 18
+//
+// +kubebuilder:validation:XValidation:rule=`!has(self.?config.parameters.ssl_groups) || self.postgresVersion > 17`,message=`The ssl_groups parameter is only available in pg18 and greater`
+//
 // # Postgres Logging
 //
 // +kubebuilder:validation:XValidation:fieldPath=`.config.parameters.log_directory`,message=`all instances need "volumes.temp" to log in "/pgtmp"`,rule=`self.?config.parameters.log_directory.optMap(v, type(v) != string || !v.startsWith("/pgtmp/logs/postgres") || self.instances.all(i, i.?volumes.temp.hasValue())).orValue(true)`
